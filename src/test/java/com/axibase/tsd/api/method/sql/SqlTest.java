4acc390cb1903f95eae622fb57dc8c5cc16f98ee
shmgrinsky <unrealwork@gmail.com>
2016-07-12 12:40:04 +0300

    simplify assertion message  in assertTableRows

--- src/test/java/com/axibase/tsd/api/method/sql/SqlTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/SqlTest.java
@@ -9,6 +9,6 @@ import static org.junit.Assert.assertEquals;
  */
 public class SqlTest extends SqlMethod {
     public static void assertTableRows(List<List<String>> row1, List<List<String>> row2) {
-        assertEquals(String.format("Rows %s and %s must  be identical", row1, row2), row1, row2);
+        assertEquals("Table rows  must  be identical", row1, row2);
     }
 }
