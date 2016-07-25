e7788719c0e0666585a265a963606570ad36b818
nikitachrs <nikitachrs@me.com>
2016-07-25 16:04:14 +0300

    Retest with TestNG

--- src/test/java/com/axibase/tsd/api/method/entity/EntityCommandTest.java
+++ src/test/java/com/axibase/tsd/api/method/entity/EntityCommandTest.java
@@ -1,20 +1,18 @@
-
 package com.axibase.tsd.api.method.entity;
 
 import com.axibase.tsd.api.model.entity.Entity;
-import org.junit.Test;
 import javax.ws.rs.core.Response;
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
-import static org.junit.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertEquals;
 
-public class EntityCommandTest extends EntityMethod {
+public class EntityCommandTest extends EntityMethod{
 
     /**
      * Issue #3111
      */
 
-    @Test
-    public void testAddNewEntityTagsForNewEntity() throws Exception {
+    @org.testng.annotations.Test
+    public void testTestAddNewEntityTagsForNewEntity() throws Exception {
         Entity EntityNameForTags = new Entity("e-for-test-add-tags");
         createOrReplaceEntity(EntityNameForTags);
         EntityNameForTags.addTag("e-tag-1", "e-val-1");
@@ -24,14 +22,13 @@ public class EntityCommandTest extends EntityMethod {
         Entity EntityNameForTagsGetToEnt = EntityNameForTagsGet.readEntity(Entity.class);
 
         assertEquals("Method should fail if new entity tags don't add for existing entity ",EntityNameForTags.getTags(), EntityNameForTagsGetToEnt.getTags());
-
     }
 
     /**
      * Issue #3111
      */
 
-    @Test
+    @org.testng.annotations.Test
     public void testUpdateEntityTagsForExistEntity() throws Exception {
         Entity EntityNameForTags = new Entity("e-for-test-update-tags");
         EntityNameForTags.addTag("e-tag-1", "e-val-1");
@@ -50,7 +47,7 @@ public class EntityCommandTest extends EntityMethod {
      * Issue #3111
      */
 
-    @Test
+    @org.testng.annotations.Test
     public void testNewEntityTagsForExistEntity() throws Exception {
         Entity EntityNameForTags = new Entity("ent-for-test-add-tags");
         EntityNameForTags.addTag("e-tag-1", "e-val-1");
@@ -60,20 +57,17 @@ public class EntityCommandTest extends EntityMethod {
         Entity EntityNameForTagsGetToEnt = EntityNameForTagsGet.readEntity(Entity.class);
 
         assertEquals("Method should fail if new entity tags don't create",EntityNameForTags.getTags(), EntityNameForTagsGetToEnt.getTags());
-
     }
 
     /**
      * Issue #3111
      */
 
-    @Test
+    @org.testng.annotations.Test
     public void testAddNewEntityTagsMailformedForNewEntity() throws Exception {
         Entity EntityNameForTags = new Entity("ent-for-test-add-tags-mailformed");
         EntityNameForTags.addTag("hello 1", "world");
         assertEquals("EntityTag name contains whitespace. Insert Entity tag is failed.", BAD_REQUEST.getStatusCode(), createOrReplaceEntity(EntityNameForTags).getStatus());
     }
+
 }
-
-
-
