2446159f0840506bd668d67b5cae699b0b4f80c7
KutuzovGregory <morgot.gregory@gmail.com>
2016-08-23 14:18:32 +0300

    code review #2

--- src/test/java/com/axibase/tsd/api/method/metric/MetricUpdateTest.java
+++ src/test/java/com/axibase/tsd/api/method/metric/MetricUpdateTest.java
@@ -14,14 +14,18 @@ import static org.testng.AssertJUnit.assertTrue;
 
 public class MetricUpdateTest extends MetricMethod {
 
-    /* #1278 */
+    /**
+     * #1278
+     **/
     @Test
     public void testMetricNameContainsWhiteSpace() throws Exception {
         final Metric metric = new Metric("update metric-1");
         assertEquals("Method should fail if metricName contains whitespace", BAD_REQUEST.getStatusCode(), updateMetric(metric).getStatus());
     }
 
-    /* #1278 */
+    /**
+     * #1278
+     **/
     @Test
     public void testMetricNameContainsSlash() throws Exception {
         final Metric metric = new Metric("update/metric-2");
@@ -33,7 +37,9 @@ public class MetricUpdateTest extends MetricMethod {
         assertTrue("Can not find required metric", metricExist(metric));
     }
 
-    /* #1278 */
+    /**
+     * #1278
+     **/
     @Test
     public void testMetricNameContainsCyrillic() throws Exception {
         final Metric metric = new Metric("updateйёmetric-3");
@@ -51,48 +57,36 @@ public class MetricUpdateTest extends MetricMethod {
         assertEquals("Unknown metric should return NotFound", NOT_FOUND.getStatusCode(), updateMetric(metric).getStatus());
     }
 
-    /* #3141 */
+    /**
+     * #3141
+     **/
     @Test
     public void testMetricTagNameIsLowerCased() throws Exception {
-        final String FIRST_TAG_NAME = "SoMeTaG";
-        final String SECOND_TAG_NAME = "NeWtAg";
+        final String TAG_NAME = "NeWtAg";
         final String TAG_VALUE = "value";
 
-        Metric metric = new Metric("metric-with-tag");
+        Metric metric = new Metric("update-metric-with-tag");
+        Response createResponse = createOrReplaceMetric(metric);
+        assertEquals("Failed to create metric", OK.getStatusCode(), createResponse.getStatus());
+
+        createOrReplaceMetricCheck(metric);
+
         Map<String, String> tags = new HashMap<>();
-        tags.put(FIRST_TAG_NAME, TAG_VALUE);
+        tags.put("NeWtAg", "value");
         metric.setTags(tags);
-        Response response1 = createOrReplaceMetric(metric);
-        assertEquals("Failed to create metric", OK.getStatusCode(), response1.getStatus());
 
-        Response response2 = queryMetric(metric.getName());
-        Metric createdMetric = response2.readEntity(Metric.class);
+        Response updateResponse = updateMetric(metric);
+        assertEquals("Failed to update metric", OK.getStatusCode(), updateResponse.getStatus());
 
-        assertEquals("Wrong metric tags count", createdMetric.getTags().size(), 1);
-        assertTrue("Metric has not tag '"+FIRST_TAG_NAME.toLowerCase()+"'",
-                createdMetric.getTags().containsKey(FIRST_TAG_NAME.toLowerCase()));
-        assertEquals("Wrong tag '"+FIRST_TAG_NAME.toLowerCase()+"' value",
-                createdMetric.getTags().get(FIRST_TAG_NAME.toLowerCase()), TAG_VALUE);
+        Response queryResponse = queryMetric(metric.getName());
+        assertEquals("Failed to query metric", OK.getStatusCode(), queryResponse.getStatus());
+        Metric updatedMetric = queryResponse.readEntity(Metric.class);
 
-        Map<String, String> newTag = new HashMap<>();
-        newTag.put("NeWtAg", "value");
+        assertEquals("Wrong metric name", metric.getName(), updatedMetric.getName());
 
-        metric.setTags(newTag);
+        Map<String, String> expectedTags = new HashMap<>();
+        expectedTags.put(TAG_NAME.toLowerCase(), TAG_VALUE);
 
-        Response response3 = updateMetric(metric);
-        assertEquals("Failed to update metric", OK.getStatusCode(), response3.getStatus());
-
-        Response response4 = queryMetric(metric.getName());
-        Metric updatedMetric = response4.readEntity(Metric.class);
-
-        assertEquals("Wrong metric tags count", updatedMetric.getTags().size(), 2);
-        assertTrue("Metric has not tag '"+FIRST_TAG_NAME.toLowerCase()+"'",
-                updatedMetric.getTags().containsKey(FIRST_TAG_NAME.toLowerCase()));
-        assertEquals("Wrong tag 'sometag' value",
-                updatedMetric.getTags().get(FIRST_TAG_NAME.toLowerCase()), TAG_VALUE);
-        assertTrue("Metric has not tag '" + SECOND_TAG_NAME.toLowerCase()+"'",
-                updatedMetric.getTags().containsKey(SECOND_TAG_NAME.toLowerCase()));
-        assertEquals("Wrong tag '"+SECOND_TAG_NAME.toLowerCase()+"' value",
-                updatedMetric.getTags().get(SECOND_TAG_NAME.toLowerCase()), TAG_VALUE);
+        assertEquals("Wrong metric tags", expectedTags, updatedMetric.getTags());
     }
 }
