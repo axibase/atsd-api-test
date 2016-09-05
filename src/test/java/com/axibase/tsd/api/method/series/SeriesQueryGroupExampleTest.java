5416838d9156649839c4d1bd1f188e2d6ab168f5
Sild <korchagin.dmitry@icloud.com>
2016-09-05 10:35:47 +0300

    add extend=false\null tests

--- src/test/java/com/axibase/tsd/api/method/series/SeriesQueryGroupExampleTest.java
+++ src/test/java/com/axibase/tsd/api/method/series/SeriesQueryGroupExampleTest.java
@@ -8,10 +8,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.testng.AssertJUnit.assertEquals;
 
@@ -124,36 +121,7 @@ public class SeriesQueryGroupExampleTest extends SeriesMethod {
         assertEquals("Grouped series do not match to expected", expected, actual);
     }
 
-    /**
-     * #2995
-     * https://github.com/axibase/atsd-docs/blob/master/api/data/series/group.md#extend
-     */
-    @Test
-    public void testExampleSumExtend() throws Exception {
-        SeriesQuery query = prepareDefaultQuery("2016-06-25T08:00:01Z", "2016-06-25T08:01:00Z");
 
-        Group group = new Group(GroupType.SUM);
-        group.setInterpolate(new Interpolate(true));
-
-        query.setGroup(group);
-
-        List<Sample> expectedSamples = Arrays.asList(
-                new Sample("2016-06-25T08:00:05.000Z", "11.0"),
-                new Sample("2016-06-25T08:00:10.000Z", "13.0"),
-                new Sample("2016-06-25T08:00:15.000Z", "16.0"),
-                new Sample("2016-06-25T08:00:30.000Z", "16.0"),
-                new Sample("2016-06-25T08:00:45.000Z", "20.0"),
-                new Sample("2016-06-25T08:00:59.000Z", "24.0")
-        );
-
-        List<Series> groupedSeries = executeQueryReturnSeries(query);
-        assertEquals("Response should contain only one series", 1, groupedSeries.size());
-        List<Sample> givenSamples = groupedSeries.get(0).getData();
-
-        final String actual = jacksonMapper.writeValueAsString(givenSamples);
-        final String expected = jacksonMapper.writeValueAsString(expectedSamples);
-        assertEquals("Grouped series do not match to expected", expected, actual);
-    }
 
     /**
      * #2995
@@ -295,6 +263,102 @@ public class SeriesQueryGroupExampleTest extends SeriesMethod {
         assertEquals("Grouped series do not match to expected", expected, actual);
     }
 
+    /**
+     * #2995
+     * https://github.com/axibase/atsd-docs/blob/master/api/data/series/group.md#extend
+     */
+    @Test
+    public void testExampleSumExtend() throws Exception {
+        SeriesQuery query = prepareDefaultQuery("2016-06-25T08:00:01Z", "2016-06-25T08:01:00Z");
+
+        Group group = new Group(GroupType.SUM);
+        group.setInterpolate(new Interpolate(true));
+
+        query.setGroup(group);
+
+        List<Sample> expectedSamples = Arrays.asList(
+                new Sample("2016-06-25T08:00:05.000Z", "11.0"),
+                new Sample("2016-06-25T08:00:10.000Z", "13.0"),
+                new Sample("2016-06-25T08:00:15.000Z", "16.0"),
+                new Sample("2016-06-25T08:00:30.000Z", "16.0"),
+                new Sample("2016-06-25T08:00:45.000Z", "20.0"),
+                new Sample("2016-06-25T08:00:59.000Z", "24.0")
+        );
+
+        List<Series> groupedSeries = executeQueryReturnSeries(query);
+        assertEquals("Response should contain only one series", 1, groupedSeries.size());
+        List<Sample> givenSamples = groupedSeries.get(0).getData();
+
+        final String actual = jacksonMapper.writeValueAsString(givenSamples);
+        final String expected = jacksonMapper.writeValueAsString(expectedSamples);
+        assertEquals("Grouped series do not match to expected", expected, actual);
+    }
+
+
+
+
+    /**
+     * #2997
+     * https://github.com/axibase/atsd-docs/blob/master/api/exampleData/series/group.md#no-aggregation
+     */
+    @Test
+    public void testExampleSumExtendFalse() throws Exception {
+        SeriesQuery query = prepareDefaultQuery("2016-06-25T08:00:00Z", "2016-06-25T08:01:00Z");
+
+        Group group = new Group(GroupType.SUM);
+        group.setInterpolate(new Interpolate(false));
+
+        query.setGroup(group);
+
+        List<Sample> expectedSamples = Arrays.asList(
+                new Sample("2016-06-25T08:00:00.000Z", "12.0"),
+                new Sample("2016-06-25T08:00:05.000Z", "3.0"),
+                new Sample("2016-06-25T08:00:10.000Z", "5.0"),
+                new Sample("2016-06-25T08:00:15.000Z", "16.0"),
+                new Sample("2016-06-25T08:00:30.000Z", "16.0"),
+                new Sample("2016-06-25T08:00:45.000Z", "20.0"),
+                new Sample("2016-06-25T08:00:59.000Z", "19.0")
+        );
+
+        List<Series> groupedSeries = executeQueryReturnSeries(query);
+        assertEquals("Response should contain only one series", 1, groupedSeries.size());
+        List<Sample> givenSamples = groupedSeries.get(0).getData();
+
+        final String actual = jacksonMapper.writeValueAsString(givenSamples);
+        final String expected = jacksonMapper.writeValueAsString(expectedSamples);
+        assertEquals("Grouped series do not match to expected", expected, actual);
+    }
+
+    /**
+     * #2997
+     * https://github.com/axibase/atsd-docs/blob/master/api/exampleData/series/group.md#no-aggregation
+     */
+    @Test
+    public void testExampleSumExtendNull() throws Exception {
+        SeriesQuery query = prepareDefaultQuery("2016-06-25T08:00:00Z", "2016-06-25T08:01:00Z");
+
+        Group group = new Group(GroupType.SUM);
+        group.setInterpolate(new Interpolate((Boolean)null));
+        query.setGroup(group);
+
+        List<Sample> expectedSamples = Arrays.asList(
+                new Sample("2016-06-25T08:00:00.000Z", "12.0"),
+                new Sample("2016-06-25T08:00:05.000Z", "3.0"),
+                new Sample("2016-06-25T08:00:10.000Z", "5.0"),
+                new Sample("2016-06-25T08:00:15.000Z", "16.0"),
+                new Sample("2016-06-25T08:00:30.000Z", "16.0"),
+                new Sample("2016-06-25T08:00:45.000Z", "20.0"),
+                new Sample("2016-06-25T08:00:59.000Z", "19.0")
+        );
+
+        List<Series> groupedSeries = executeQueryReturnSeries(query);
+        assertEquals("Response should contain only one series", 1, groupedSeries.size());
+        List<Sample> givenSamples = groupedSeries.get(0).getData();
+
+        final String actual = jacksonMapper.writeValueAsString(givenSamples);
+        final String expected = jacksonMapper.writeValueAsString(expectedSamples);
+        assertEquals("Grouped series do not match to expected", expected, actual);
+    }
 
     private SeriesQuery prepareDefaultQuery(String startDate, String endDate) {
         SeriesQuery seriesQuery = new SeriesQuery();
@@ -304,6 +368,7 @@ public class SeriesQueryGroupExampleTest extends SeriesMethod {
         seriesQuery.setEndDate(endDate);
         return seriesQuery;
     }
+
 
     private static class SeriesGroupExampleStructure {
         private String entity;
