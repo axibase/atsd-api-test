30d14976959ef2a42aee9b58cdbea780efc5b5b7
unrealwork <unrealwork@gmail.com>
2016-09-26 09:02:26 +0000

    added join and filtering tests

--- src/test/java/com/axibase/tsd/api/method/sql/clause/with/InterpolationTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/clause/with/InterpolationTest.java
@@ -773,8 +773,226 @@ public class InterpolationTest extends SqlTest {
         //Assert
         assertTableRowsExist(expectedRows, resultTable);
     }
-    
-    
+
+    @Test
+    private void testLinearJoin() throws Exception {
+        //Create data for test
+        String testMetricName1 = testNameGenerator.getMetricName();
+        String testMetricName2 = testNameGenerator.getMetricName();
+        String testEntityName = testNameGenerator.getEntityName();
+        Series series1 = new Series();
+        series1.setMetric(testMetricName1);
+        series1.setEntity(testEntityName);
+        series1.setData(Arrays.asList(
+                new Sample("2016-06-19T11:00:03.000Z", "1"),
+                new Sample("2016-06-19T11:08:03.000Z", "4"),
+                new Sample("2016-06-19T11:09:15.000Z", "5"),
+                new Sample("2016-06-19T11:13:33.000Z", "34")
+        ));
+        Series series2 = new Series();
+        series2.setMetric(testMetricName2);
+        series2.setEntity(testEntityName);
+        series2.setData(Arrays.asList(
+                new Sample("2016-06-19T11:00:03.000Z", "2"),
+                new Sample("2016-06-19T11:08:03.000Z", "3"),
+                new Sample("2016-06-19T11:09:15.000Z", "4"),
+                new Sample("2016-06-19T11:13:33.000Z", "7")
+                )
+        );
+        SeriesMethod.insertSeriesCheck(Arrays.asList(series1, series2));
+        //Generate expected expected rows
+        String[][] expectedRows = {
+                {"2016-06-19T11:08:00.000Z", "3.98125", "2.99375"},
+                {"2016-06-19T11:09:00.000Z", "4.791666666666667", "3.7916666666666665"},
+                {"2016-06-19T11:10:00.000Z", "10.058139534883722", "4.523255813953488"},
+                {"2016-06-19T11:11:00.000Z", "16.80232558139535", "5.220930232558139"},
+                {"2016-06-19T11:12:00.000Z", "23.546511627906977", "5.9186046511627906"},
+                {"2016-06-19T11:13:00.000Z", "30.290697674418606", "6.616279069767442"}
+
+        };
+
+        //Query data
+        String sqlQuery = String.format("SELECT t1.datetime as 'datetime', t1.value, t2.value FROM '%s' t1%nJOIN '%s' t2\n" +
+                        "WHERE t1.datetime >= '2016-06-19T11:08:00.000Z' " +
+                        "AND t1.datetime < '2016-06-19T11:15:00.000Z'%nWITH INTERPOLATE(1 MINUTE, LINEAR)",
+                testMetricName1, testMetricName2);
+        StringTable resultTable = executeQuery(sqlQuery).readEntity(StringTable.class);
+        //Assert
+        assertTableRowsExist(expectedRows, resultTable);
+    }
+
+    @Test
+    private void testNoneJoin() throws Exception {
+        //Create data for test
+        String testMetricName1 = testNameGenerator.getMetricName();
+        String testMetricName2 = testNameGenerator.getMetricName();
+        String testEntityName = testNameGenerator.getEntityName();
+        Series series1 = new Series();
+        series1.setMetric(testMetricName1);
+        series1.setEntity(testEntityName);
+        series1.setData(Arrays.asList(
+                new Sample("2016-06-19T11:00:03.000Z", "1"),
+                new Sample("2016-06-19T11:08:03.000Z", "4"),
+                new Sample("2016-06-19T11:09:15.000Z", "5"),
+                new Sample("2016-06-19T11:13:33.000Z", "34")
+        ));
+        Series series2 = new Series();
+        series2.setMetric(testMetricName2);
+        series2.setEntity(testEntityName);
+        series2.setData(Arrays.asList(
+                new Sample("2016-06-19T11:00:03.000Z", "2"),
+                new Sample("2016-06-19T11:08:03.000Z", "3"),
+                new Sample("2016-06-19T11:09:15.000Z", "4"),
+                new Sample("2016-06-19T11:13:33.000Z", "7")
+                )
+        );
+        SeriesMethod.insertSeriesCheck(Arrays.asList(series1, series2));
+        //Generate expected expected rows
+        String[][] expectedRows = {
+                {"2016-06-19T11:08:00.000Z", "4", "3"},
+                {"2016-06-19T11:09:00.000Z", "4.791666666666667", "3.7916666666666665"},
+                {"2016-06-19T11:10:00.000Z", "10.058139534883722", "4.523255813953488"},
+                {"2016-06-19T11:11:00.000Z", "16.80232558139535", "5.220930232558139"},
+                {"2016-06-19T11:12:00.000Z", "23.546511627906977", "5.9186046511627906"},
+                {"2016-06-19T11:13:00.000Z", "30.290697674418606", "6.616279069767442"}
+
+        };
+
+        //Query data
+        String sqlQuery = String.format("SELECT t1.datetime as 'datetime', t1.value, t2.value FROM '%s' t1%nJOIN '%s' t2%n" +
+                        "WHERE t1.datetime >= '2016-06-19T11:08:00.000Z' " +
+                        "AND t1.datetime < '2016-06-19T11:15:00.000Z'%nWITH INTERPOLATE(1 MINUTE)",
+                testMetricName1, testMetricName2);
+        StringTable resultTable = executeQuery(sqlQuery).readEntity(StringTable.class);
+        //Assert
+        assertTableRowsExist(expectedRows, resultTable);
+    }
+
+    @Test
+    private void testExtendJoin() throws Exception {
+        //Create data for test
+        String testMetricName1 = testNameGenerator.getMetricName();
+        String testMetricName2 = testNameGenerator.getMetricName();
+        String testEntityName = testNameGenerator.getEntityName();
+        Series series1 = new Series();
+        series1.setMetric(testMetricName1);
+        series1.setEntity(testEntityName);
+        series1.setData(Arrays.asList(
+                new Sample("2016-06-19T11:00:03.000Z", "1"),
+                new Sample("2016-06-19T11:08:03.000Z", "4"),
+                new Sample("2016-06-19T11:09:15.000Z", "5"),
+                new Sample("2016-06-19T11:13:33.000Z", "34")
+        ));
+        Series series2 = new Series();
+        series2.setMetric(testMetricName2);
+        series2.setEntity(testEntityName);
+        series2.setData(Arrays.asList(
+                new Sample("2016-06-19T11:00:03.000Z", "2"),
+                new Sample("2016-06-19T11:08:03.000Z", "3"),
+                new Sample("2016-06-19T11:09:15.000Z", "4"),
+                new Sample("2016-06-19T11:13:33.000Z", "7")
+                )
+        );
+        SeriesMethod.insertSeriesCheck(Arrays.asList(series1, series2));
+        //Generate expected expected rows
+        String[][] expectedRows = {
+                {"2016-06-19T11:08:00.000Z", "4", "3"},
+                {"2016-06-19T11:09:00.000Z", "4.791666666666667", "3.7916666666666665"},
+                {"2016-06-19T11:10:00.000Z", "10.058139534883722", "4.523255813953488"},
+                {"2016-06-19T11:11:00.000Z", "16.80232558139535", "5.220930232558139"},
+                {"2016-06-19T11:12:00.000Z", "23.546511627906977", "5.9186046511627906"},
+                {"2016-06-19T11:13:00.000Z", "30.290697674418606", "6.616279069767442"},
+                {"2016-06-19T11:14:00.000Z", "34", "7"}
+
+        };
+
+        //Query data
+        String sqlQuery = String.format("SELECT t1.datetime as 'datetime', t1.value, t2.value FROM '%s' t1%nJOIN '%s' t2%n" +
+                        "WHERE t1.datetime >= '2016-06-19T11:08:00.000Z' " +
+                        "AND t1.datetime < '2016-06-19T11:15:00.000Z'%nWITH INTERPOLATE(1 MINUTE, EXTEND)",
+                testMetricName1, testMetricName2);
+        StringTable resultTable = executeQuery(sqlQuery).readEntity(StringTable.class);
+        //Assert
+        assertTableRowsExist(expectedRows, resultTable);
+    }
+
+    @Test
+    private void testPriorJoin() throws Exception {
+        //Create data for test
+        String testMetricName1 = testNameGenerator.getMetricName();
+        String testMetricName2 = testNameGenerator.getMetricName();
+        String testEntityName = testNameGenerator.getEntityName();
+        Series series1 = new Series();
+        series1.setMetric(testMetricName1);
+        series1.setEntity(testEntityName);
+        series1.setData(Arrays.asList(
+                new Sample("2016-06-19T11:00:03.000Z", "1"),
+                new Sample("2016-06-19T11:08:03.000Z", "4"),
+                new Sample("2016-06-19T11:09:15.000Z", "5"),
+                new Sample("2016-06-19T11:13:33.000Z", "34")
+        ));
+        Series series2 = new Series();
+        series2.setMetric(testMetricName2);
+        series2.setEntity(testEntityName);
+        series2.setData(Arrays.asList(
+                new Sample("2016-06-19T11:00:03.000Z", "2"),
+                new Sample("2016-06-19T11:08:03.000Z", "3"),
+                new Sample("2016-06-19T11:09:15.000Z", "4"),
+                new Sample("2016-06-19T11:13:33.000Z", "7")
+                )
+        );
+        SeriesMethod.insertSeriesCheck(Arrays.asList(series1, series2));
+        //Generate expected expected rows
+        String[][] expectedRows = {
+                {"2016-06-19T11:08:00.000Z", "1", "2"},
+                {"2016-06-19T11:09:00.000Z", "4.791666666666667", "3.7916666666666665"},
+                {"2016-06-19T11:10:00.000Z", "10.058139534883722", "4.523255813953488"},
+                {"2016-06-19T11:11:00.000Z", "16.80232558139535", "5.220930232558139"},
+                {"2016-06-19T11:12:00.000Z", "23.546511627906977", "5.9186046511627906"},
+                {"2016-06-19T11:13:00.000Z", "30.290697674418606", "6.616279069767442"},
+                {"2016-06-19T11:14:00.000Z", "34", "7"}
+
+        };
+
+        //Query data
+        String sqlQuery = String.format("SELECT t1.datetime as 'datetime', t1.value, t2.value FROM '%s' t1%nJOIN '%s' t2%n" +
+                        "WHERE t1.datetime >= '2016-06-19T11:08:00.000Z' " +
+                        "AND t1.datetime < '2016-06-19T11:15:00.000Z'%nWITH INTERPOLATE(1 MINUTE, PRIOR)",
+                testMetricName1, testMetricName2);
+        StringTable resultTable = executeQuery(sqlQuery).readEntity(StringTable.class);
+        //Assert
+        assertTableRowsExist(expectedRows, resultTable);
+    }
+
+    @Test
+    private void testFiltering() throws Exception {
+        //Create data for test
+        String testEntityName = testNameGenerator.getEntityName();
+        String testMetricName = testNameGenerator.getMetricName();
+        Series series = new Series(testEntityName, testMetricName);
+        series.setData(Arrays.asList(
+                new Sample("2016-06-19T11:00:03.000Z", "1"),
+                new Sample("2016-06-19T11:08:03.000Z", "4"),
+                new Sample("2016-06-19T11:09:15.000Z", "5"),
+                new Sample("2016-06-19T11:13:33.000Z", "34")
+                )
+        );
+        SeriesMethod.insertSeriesCheck(Collections.singletonList(series));
+        //Generate expected expected rows
+        String[][] expectedRows = {
+                {"2016-06-19T11:08:00.000Z", "4"},
+                {"2016-06-19T11:09:00.000Z", "4.791666666666667"}
+        };
+        //Query data
+        String sqlQuery = String.format("SELECT datetime, value FROM '%s'%nWHERE datetime >= '2016-06-19T11:08:00.000Z' " +
+                        "AND datetime < '2016-06-19T11:15:00.000Z' AND value < 6%nWITH INTERPOLATE(1 MINUTE, EXTEND)",
+                testMetricName);
+        StringTable resultTable = executeQuery(sqlQuery).readEntity(StringTable.class);
+        //Assert
+        assertTableRowsExist(expectedRows, resultTable);
+    }
+
+
     private List<Sample> interpolatedValues(PolynomialSplineFunction inteterpolateFunction, Long startTime, Long endTime, Long period) {
         List<Sample> result = new ArrayList<>();
         double[] knots = inteterpolateFunction.getKnots();
