9b61bfd8e31c9a0da1377396f7aa0a64b15699a2
unrealwork <unrealwork@gmail.com>
2016-10-06 11:43:13 +0300

    simplify command objects

--- src/test/java/com/axibase/tsd/api/model/command/AbstractCommand.java
+++ src/test/java/com/axibase/tsd/api/model/command/AbstractCommand.java
@@ -2,27 +2,18 @@ package com.axibase.tsd.api.model.command;
 
 
 public abstract class AbstractCommand implements PlainCommand {
-    private StringBuilder sb;
     private String commandText;
 
     public AbstractCommand(String commandText) {
-        sb = new StringBuilder(commandText.concat(" "));
         this.commandText = commandText;
     }
 
     @Override
     public String compose() {
-        return sb.toString();
+        return toString();
     }
 
-    protected void appendField(String field, String type) {
-        sb.append(String.format("%s:%s ", field, type));
-    }
-
-    protected abstract void build();
-
-    protected void clean() {
-        sb.setLength(0);
-        sb.append(commandText.concat(" "));
+    protected StringBuilder commandBuilder() {
+        return new StringBuilder(String.format("%s ", commandText));
     }
 }
