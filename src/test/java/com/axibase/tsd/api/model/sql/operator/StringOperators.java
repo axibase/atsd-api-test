d99e39d19a356ca510a42fff5b5fb5c55ab82b48
unrealwork <unrealwork@gmail.com>
2016-10-26 14:45:55 +0300

    apply test

---
+++ src/test/java/com/axibase/tsd/api/model/sql/operator/StringOperators.java
@@ -0,0 +1,33 @@
+package com.axibase.tsd.api.model.sql.operator;
+
+
+public class StringOperators {
+    public enum Unary {
+        ISNULL("IS NULL"), ISNOTNULL("IS NOT NULL");
+        private String text;
+
+        Unary(String text) {
+            this.text = text;
+        }
+
+        @Override
+        public String toString() {
+            return text;
+        }
+    }
+
+    public enum Binary {
+        EQUALS("="), NOT_EQUALS("!="), NOT_EQUALS_ALTER("<>"), LESS("<"), LESS_OR_EQUALS("<="), GREATER(">"),
+        GREATER_OR_EQUALS(">="), LIKE("LIKE"), REGEX("REGEX");
+        private String text;
+
+        Binary(String text) {
+            this.text = text;
+        }
+
+        @Override
+        public String toString() {
+            return text;
+        }
+    }
+}
