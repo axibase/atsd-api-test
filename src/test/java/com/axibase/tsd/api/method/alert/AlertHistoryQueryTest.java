42d50d7326010ad02c22876e17664050843ffdc8
Aleksandr Veselov <veselov143@gmail.com>
2016-11-29 10:07:16 +0300

    SQL: Test alert history query does not throw NullPointerException

--- src/test/java/com/axibase/tsd/api/method/alert/AlertHistoryQueryTest.java
+++ src/test/java/com/axibase/tsd/api/method/alert/AlertHistoryQueryTest.java
@@ -9,15 +9,16 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import javax.ws.rs.core.Response;
 import java.util.Collections;
 import java.util.List;
 
 import static com.axibase.tsd.api.util.Mocks.MAX_QUERYABLE_DATE;
 import static com.axibase.tsd.api.util.Mocks.MIN_QUERYABLE_DATE;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
-@Test(enabled = false)
+@Test
 public class AlertHistoryQueryTest extends AlertTest {
     private final static String ALERTHISTORY_ENTITY_NAME = "alert-historyquery-entity-1";
 
@@ -60,7 +61,7 @@ public class AlertHistoryQueryTest extends AlertTest {
                 "Query response must contain at least one alert. Query %s",
                 query
         );
-        assertTrue(alertList.size() > 0, assertMessage);
+        assertTrue(assertMessage, alertList.size() > 0);
     }
 
     /**
@@ -71,7 +72,46 @@ public class AlertHistoryQueryTest extends AlertTest {
         AlertHistoryQuery qExist = templateQuery().setEntity("alert-historyquery-entity-1");
         AlertHistoryQuery qUnknown = templateQuery().setEntity("UNKNOWN");
         List<Alert> resultList = AlertMethod.queryHistory(qExist, qUnknown);
-        assertEquals(resultList.size(), 2, "Fail to get alert history by queries with unknown entity");
-        assertEquals("ENTITY not found for name: 'unknown'", resultList.get(1).getWarning(), "Unexpected warning message");
+        assertEquals("Fail to get alert history by queries with unknown entity", 2, resultList.size());
+        assertEquals("Unexpected warning message", "ENTITY not found for name: 'unknown'", resultList.get(1).getWarning());
+    }
+
+    @DataProvider(name = "lotsOfNullProvider")
+    public Object[][] provideLotsOfNullResponse() {
+
+        return new Object[][] {
+            {"[\n" +
+             "  {\n" +
+             "    \"entity\": \"*\",\n" +
+             "    \"entities\": null,\n" +
+             "    \"entityExpression\": null,\n" +
+             "    \"entityGroup\": null,\n" +
+             "    \"rule\": null,\n" +
+             "    \"metric\": null,\n" +
+             "    \"startDate\": \"1000-01-01T00:00:00.000Z\",\n" +
+             "    \"endDate\": \"9999-12-31T23:59:59.999Z\",\n" +
+             "    \"interval\": null,\n" +
+             "    \"limit\": null\n" +
+             "  }\n" +
+             "]"}
+        };
+    }
+
+    /**
+     * #3640
+     */
+    @Test(dataProvider = "lotsOfNullProvider")
+    public void testQueryWithNullFieldsSuccess(String json) throws Exception {
+        Response resp = AlertMethod.queryHistoryResponseRawJSON(json);
+        assertNotThrowsNullPointerException(resp);
+    }
+
+    public static void assertNotThrowsNullPointerException(Response resp) throws Exception {
+        if (resp.getStatusInfo().getFamily() == Response.Status.Family.SUCCESSFUL) {
+            return;
+        }
+        String errMsg = extractErrorMessage(resp);
+        assertEquals("Unexpected exception thrown","NullPointerException", errMsg);
+        assertTrue("NullPointerException thrown", !"NullPointerException".equals(errMsg));
     }
 }
