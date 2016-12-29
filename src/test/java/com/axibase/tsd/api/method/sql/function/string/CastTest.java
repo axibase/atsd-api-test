75cec534b81648af156a3e1717773e8e3e925939
RawrLion <cukaleksandr@yandex.ru>
2016-12-29 13:28:45 +0300

    moved to sql.function.string renamed to CastTest edited 1 line in SqlTest

--- src/test/java/com/axibase/tsd/api/method/sql/function/string/CastTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/function/string/CastTest.java
@@ -1,4 +1,4 @@
-package com.axibase.tsd.api.method.sql.function.cast;
+package com.axibase.tsd.api.method.sql.function.string;
 
 import com.axibase.tsd.api.method.series.SeriesMethod;
 import com.axibase.tsd.api.method.sql.SqlTest;
@@ -12,7 +12,7 @@ import java.util.Arrays;
 import static com.axibase.tsd.api.util.Util.TestNames.entity;
 import static com.axibase.tsd.api.util.Util.TestNames.metric;
 
-public class SqlCastTest extends SqlTest {
+public class CastTest extends SqlTest {
     private static final String TEST_METRIC1_NAME = metric();
     private static final String TEST_METRIC2_NAME = metric();
     private static final String TEST_METRIC3_NAME = metric();
