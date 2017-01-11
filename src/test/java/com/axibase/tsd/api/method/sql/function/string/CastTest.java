cc0467743410fd2f2143734787442b077dfda34f
RawrLion <cukaleksandr@yandex.ru>
2017-01-11 15:42:24 +0300

    code review #1 -- removed redundant including

--- src/test/java/com/axibase/tsd/api/method/sql/function/string/CastTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/function/string/CastTest.java
@@ -8,7 +8,6 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
