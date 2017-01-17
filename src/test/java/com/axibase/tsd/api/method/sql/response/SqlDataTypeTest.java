6366b681cafaaa30ae38421a0fb048fabc3e888d
Valery Tolstov <me@vtolstov.org>
2017-01-17 10:53:06 +0300

    Apply requsted changes
    - Use mocks
    - Register entity
    - Rename constants
    - Various simplifications

--- src/test/java/com/axibase/tsd/api/method/sql/response/SqlDataTypeTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/response/SqlDataTypeTest.java
@@ -5,83 +5,77 @@ import com.axibase.tsd.api.method.series.SeriesMethod;
 import com.axibase.tsd.api.method.sql.SqlMethod;
 import com.axibase.tsd.api.model.metric.Metric;
 import com.axibase.tsd.api.model.series.DataType;
-import com.axibase.tsd.api.model.series.Sample;
 import com.axibase.tsd.api.model.series.Series;
 import com.axibase.tsd.api.model.sql.StringTable;
+import com.axibase.tsd.api.util.Mocks;
+import com.axibase.tsd.api.util.Registry;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.testng.AssertJUnit.assertEquals;
 
 public class SqlDataTypeTest extends SqlMethod {
 
-    private static final String entityName = "tst-type";
-    private static final String timestampString = "2017-01-12T00:00:00.000Z";
+    private static final String ENTITY_NAME = "tst-type";
+    private static final String QUERY_TEMPLATE =
+            "SELECT value FROM atsd_series WHERE metric IN (%s)";
+    private static final int VALUE_COLUMN = 0;
 
-    private static final ValueDescription shortMetric =
-            new ValueDescription("tst-metric-short", "10", DataType.SHORT);
-    private static final ValueDescription integerMetric =
-            new ValueDescription("tst-metric-integer", "10", DataType.INTEGER);
-    private static final ValueDescription longMetric =
-            new ValueDescription("tst-metric-long", "10", DataType.LONG);
-    private static final ValueDescription floatMetric =
-            new ValueDescription("tst-metric-float", "10.0", DataType.FLOAT);
-    private static final ValueDescription doubleMetric =
-            new ValueDescription("tst-metric-double", "10.0", DataType.DOUBLE);
-    private static final ValueDescription decimalMetric =
-            new ValueDescription("tst-metric-decimal", "10.0", DataType.DECIMAL);
-
-    private static final ValueDescription[] metricsData = new ValueDescription[]{
-            shortMetric, integerMetric, longMetric,
-            floatMetric, doubleMetric, decimalMetric
-    };
+    private static Map<DataType, String> typeToName = new HashMap<>();
 
     @BeforeClass
     public static void prepareData() throws Exception {
+        Registry.Entity.register(ENTITY_NAME);
+
         List<Series> seriesList = new ArrayList<>();
-        for (ValueDescription desc : metricsData) {
-            MetricMethod.createOrReplaceMetricCheck(desc.getMetric());
+        for (DataType type : DataType.values()) {
+            Metric metric = Mocks.metric();
+            metric.setDataType(type);
+            metric.setEnabled(true);
+            typeToName.put(type, metric.getName());
+            MetricMethod.createOrReplaceMetricCheck(metric);
 
             Series s = new Series();
-            s.setMetric(desc.metricName);
-            s.setEntity(entityName);
-            s.addData(new Sample(timestampString, desc.value));
+            s.setMetric(metric.getName());
+            s.setEntity(ENTITY_NAME);
+            s.addData(Mocks.SAMPLE);
             seriesList.add(s);
         }
 
-        /* Note: there are problems with number format in checker */
         SeriesMethod.insertSeriesCheck(seriesList);
     }
 
     @DataProvider(name = "testSqlType")
     public static Object[][] metricsType() {
         return new Object[][]{
-                {new StringArrayHolder(shortMetric.metricName, integerMetric.metricName, longMetric.metricName),
+                {new DataTypeArray(DataType.SHORT, DataType.INTEGER, DataType.LONG),
                         "bigint"},
 
-                {new StringArrayHolder(shortMetric.metricName, integerMetric.metricName),
+                {new DataTypeArray(DataType.SHORT, DataType.INTEGER),
                         "integer"},
 
-                {new StringArrayHolder(shortMetric.metricName),
+                {new DataTypeArray(DataType.SHORT),
                         "smallint"},
 
-                {new StringArrayHolder(shortMetric.metricName, floatMetric.metricName),
+                {new DataTypeArray(DataType.SHORT, DataType.FLOAT),
                         "decimal"},
 
-                {new StringArrayHolder(floatMetric.metricName),
+                {new DataTypeArray(DataType.FLOAT),
                         "float"},
 
-                {new StringArrayHolder(floatMetric.metricName, doubleMetric.metricName, decimalMetric.metricName),
+                {new DataTypeArray(DataType.FLOAT, DataType.DOUBLE, DataType.DECIMAL),
                         "decimal"},
 
-                {new StringArrayHolder(floatMetric.metricName, doubleMetric.metricName),
+                {new DataTypeArray(DataType.FLOAT, DataType.DOUBLE),
                         "double"},
 
-                {new StringArrayHolder(longMetric.metricName, doubleMetric.metricName),
+                {new DataTypeArray(DataType.LONG, DataType.DOUBLE),
                         "decimal"},
         };
     }
@@ -90,51 +84,28 @@ public class SqlDataTypeTest extends SqlMethod {
      * #3773
      */
     @Test(dataProvider = "testSqlType")
-    public void testDataType(StringArrayHolder arrayHolder, String expectedType) {
-        String queryTemplate =
-                "SELECT datetime, entity, metric, value\n" +
-                        " FROM atsd_series\n" +
-                        "WHERE metric IN (%s)";
-        String sqlQuery = String.format(queryTemplate, arrayHolder.toString());
-        StringTable table = queryResponse(sqlQuery).readEntity(StringTable.class);
-        String actualType = table.getColumnMetaData(3).getDataType();
+    public void testDataType(DataTypeArray selectedTypes, String expectedType) {
+        String sqlQuery = String.format(QUERY_TEMPLATE, selectedTypes.toString());
+        StringTable table = queryTable(sqlQuery);
+        String actualType = table.getColumnMetaData(VALUE_COLUMN).getDataType();
 
-        assertEquals(expectedType, actualType);
+        assertEquals("Inferred type expected to be " + expectedType + ", but got " + actualType,
+                expectedType, actualType);
     }
 
+    private static class DataTypeArray {
+        private DataType[] array;
 
-    private static class ValueDescription {
-        public String metricName;
-        public String value;
-        public DataType type;
-
-        ValueDescription(String metricName, String value, DataType type) {
-            this.metricName = metricName;
-            this.value = value;
-            this.type = type;
-        }
-
-        Metric getMetric() {
-            return new Metric(metricName).setDataType(type);
-        }
-    }
-
-    private static class StringArrayHolder {
-        private String[] array;
-
-        StringArrayHolder(String... array) {
+        DataTypeArray(DataType... array) {
             this.array = array;
         }
 
         public String toString() {
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < array.length; i++) {
-                if (i > 0) {
+                if (i > 0)
                     sb.append(", ");
-                }
-                sb.append("'");
-                sb.append(array[i]);
-                sb.append("'");
+                sb.append("'").append(typeToName.get(array[i])).append("'");
             }
             return sb.toString();
         }
