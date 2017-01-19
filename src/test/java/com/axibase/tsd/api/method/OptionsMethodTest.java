aaf3a95ccf5ff059757eceea564a9c1afed6a998
Aleksandr Veselov <veselov143@gmail.com>
2017-01-19 16:05:01 +0300

    Fixed missing request headers

--- src/test/java/com/axibase/tsd/api/method/OptionsMethodTest.java
+++ src/test/java/com/axibase/tsd/api/method/OptionsMethodTest.java
@@ -1,11 +1,11 @@
 package com.axibase.tsd.api.method;
 
 import jersey.repackaged.com.google.common.collect.Sets;
+import org.apache.commons.lang3.StringUtils;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import javax.ws.rs.core.Response;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,7 +18,7 @@ import static org.testng.AssertJUnit.assertNotNull;
 public class OptionsMethodTest extends BaseMethod {
 
     private static final Set<String> ALLOWED_ORIGINS_SET = Sets.newHashSet("*");
-    private static final Set<String> ALLOWED_METHODS_SET = Sets.newHashSet("GET", "POST", "PUT", "PATCH", "DELETE");
+    private static final Set<String> ALLOWED_METHODS_SET = Sets.newHashSet("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE");
     private static final Set<String> ALLOWED_HEADERS_SET = Sets.newHashSet("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization");
 
     @DataProvider(name = "availablePathProvider")
@@ -61,7 +61,8 @@ public class OptionsMethodTest extends BaseMethod {
                 {"/api/v1/entity-groups/group/entities/delete"},
                 {"/api/v1/version"},
                 // SQL
-                {"/api/sql"}
+                // Parameter stub is required (LOL :D)
+                {"/api/sql?q="}
         };
     }
 
@@ -70,25 +71,31 @@ public class OptionsMethodTest extends BaseMethod {
      */
     @Test(dataProvider = "availablePathProvider")
     public static void testResponseOptionsHeadersForURLs(String path) throws Exception {
-        Response response = httpRootResource.path(path).request().options();
+        Response response = httpRootResource.path(path).request()
+                .header("Access-Control-Request-Method", "GET")
+                .header("Access-Control-Request-Headers", StringUtils.join(ALLOWED_HEADERS_SET, ","))
+                .header("Origin", "itdoesntmatter")
+                .options();
 
         assertEquals("Bad response status", Response.Status.OK.getStatusCode(), response.getStatus());
 
         assertResponseContainsHeaderWithValues(ALLOWED_METHODS_SET, response, "Access-Control-Allow-Methods");
         assertResponseContainsHeaderWithValues(ALLOWED_HEADERS_SET, response, "Access-Control-Allow-Headers");
         assertResponseContainsHeaderWithValues(ALLOWED_ORIGINS_SET, response, "Access-Control-Allow-Origin");
-
     }
 
     private static void assertResponseContainsHeaderWithValues(Set<String> expected, Response response, String headerName) {
         String headerValue = response.getHeaderString(headerName);
         assertNotNull("No such header: " + headerName, headerValue);
         assertEquals(String.format("Invalid %s header value", headerName), expected, splitByComma(headerValue));
-
     }
 
     private static Set<String> splitByComma(String str) {
-        return new HashSet<>(Arrays.asList(str.split(", ")));
+        Set<String> values = new HashSet<>();
+        for (String value: str.split(",")) {
+            values.add(value.trim());
+        }
+        return values;
     }
 
 }
