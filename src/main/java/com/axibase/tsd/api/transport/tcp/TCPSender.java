55988f1c1bb6dd01d7ff7504759250e806bb77c3
Unrealwork <unrealwork@gmail.com>
2017-02-14 12:38:41 +0300

    fix sonar issues, sync with master

---
+++ src/main/java/com/axibase/tsd/api/transport/tcp/TCPSender.java
@@ -0,0 +1,96 @@
+package com.axibase.tsd.api.transport.tcp;
+
+import com.axibase.tsd.api.Checker;
+import com.axibase.tsd.api.Config;
+import com.axibase.tsd.api.method.checks.AbstractCheck;
+import com.axibase.tsd.api.model.command.PlainCommand;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+
+public class TCPSender {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TCPSender.class);
+    private static final String DEFAULT_CHARSET_ENCODING = "UTF-8";
+    private static final String DEBUG_PREFIX = "debug ";
+    private static final String LINE_SEPARATOR = System.getProperty("line.separator");
+    private static Config config;
+
+    static {
+        initialize();
+    }
+
+    private TCPSender() {
+    }
+
+    private static void initialize() {
+        try {
+            config = Config.getInstance();
+        } catch (FileNotFoundException e) {
+            LOGGER.error("Config file not found!");
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private static synchronized String send(String command, Boolean isDebugMode) throws IOException {
+        try (Socket socket = new Socket(config.getServerName(), config.getTcpPort());
+             DataOutputStream requestStream = new DataOutputStream(socket.getOutputStream());
+             BufferedReader responseStream = new BufferedReader(
+                     new InputStreamReader(socket.getInputStream(), DEFAULT_CHARSET_ENCODING)
+             )) {
+            String request = isDebugMode ? DEBUG_PREFIX.concat(command) : command;
+            IOUtils.write(request, requestStream);
+            String response = isDebugMode ? responseStream.readLine() : null;
+            LOGGER.debug(" > tcp://{}:{} \n\t{}\n< {}", config.getServerName(), config.getTcpPort(), request, response);
+            return response;
+        } catch (IOException e) {
+            LOGGER.error("Unable to send command: {} \n Host: {}\n Port");
+            throw e;
+        }
+    }
+
+    public static void send(String request) throws IOException {
+        send(request, false);
+    }
+
+    public static String send(PlainCommand command, Boolean isDebugMode) throws IOException {
+        return send(Collections.singleton(command), isDebugMode);
+    }
+
+    public static void send(PlainCommand... commands) throws IOException {
+        send(Arrays.asList(commands), false);
+    }
+
+    public static String send(Collection<? extends PlainCommand> commands, Boolean isDebugMode) throws IOException {
+        String request = buildRequest(commands);
+        return send(request, isDebugMode);
+    }
+
+    private static String buildRequest(Collection<? extends PlainCommand> commands) {
+        StringBuilder builder = new StringBuilder();
+        for (PlainCommand command : commands) {
+            builder.append(command).append(LINE_SEPARATOR);
+        }
+        return builder.toString();
+    }
+
+    public static void send(Collection<? extends PlainCommand> commands) throws IOException {
+        send(commands, false);
+    }
+
+    public static String sendChecked(AbstractCheck check, String request) throws IOException {
+        String response = send(request, false);
+        Checker.check(check);
+        return response;
+    }
+
+    public static String sendChecked(AbstractCheck check, Collection<? extends PlainCommand> commands) throws IOException {
+        return sendChecked(check, buildRequest(commands));
+    }
+}
