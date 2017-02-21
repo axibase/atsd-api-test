9f3301051c02103c05d2efead18dd7fc4013eabb
alexey-kravtsov <Havok.08@mail.ru>
2017-02-21 12:53:54 +0300

    SQL: Incorrect merge with "JOIN USING entity" #3872  - Trying to fix Travis Cl build error

--- src/test/java/com/axibase/tsd/api/method/sql/clause/join/OuterJoinMergeTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/clause/join/OuterJoinMergeTest.java
@@ -12,8 +12,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static com.axibase.tsd.api.util.Util.TestNames.entity;
-import static com.axibase.tsd.api.util.Util.TestNames.metric;
+import static com.axibase.tsd.api.util.Util.TestNames.*;
 
 public class OuterJoinMergeTest extends SqlTest {
     private static final List<String> METRIC_NAMES = Arrays.asList(metric(), metric());
