e9811e6bf41061442c835c78250b7b65b5dcb6c0
alexey-kravtsov <Havok.08@mail.ru>
2017-03-21 14:50:00 +0300

    Removed unused import

--- src/test/java/com/axibase/tsd/api/method/sql/clause/orderby/SqlOrderByNullValuesTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/clause/orderby/SqlOrderByNullValuesTest.java
@@ -7,7 +7,6 @@ import com.axibase.tsd.api.util.Mocks;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 
