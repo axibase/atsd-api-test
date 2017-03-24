32fd8f0b06193c66fb50bd017253a375735da650
Aleksandr Veselov <veselov143@gmail.com>
2017-03-24 11:37:20 +0300

    SQL:: Add tests for CASE operator in ORDER BY and WHERE  (#3852)

--- src/test/java/com/axibase/tsd/api/method/sql/operator/CaseOperatorTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/operator/CaseOperatorTest.java
@@ -47,13 +47,33 @@ public class CaseOperatorTest extends SqlTest {
 
         final String[][] expected = {
                 { "high" }, // 10
-                { "low" },  // 0
-                { "low" }   // -5
+                { "low"  },  // 0
+                { "low"  }   // -5
         };
 
         assertSqlQueryRows("Bad CASE... result", expected, sql);
     }
 
+    /**
+     * #3981
+     */
+    @Test
+    public void testCaseOperator() {
+        final String sql = String.format(
+                "SELECT CASE WHEN value > 0 THEN 'high' ELSE 'low' END%n" +
+                "FROM '%s'%n" +
+                "ORDER BY datetime ASC",
+                DEFAULT_SERIES.getMetric()
+        );
+
+        final String[][] expected = {
+                { "high" }, // 10
+                { "low"  },  // 0
+                { "low"  }   // -5
+        };
+
+        assertSqlQueryRows("Bad CASE... result", expected, sql);
+    }
 
     /**
      * #3981
@@ -87,28 +107,114 @@ public class CaseOperatorTest extends SqlTest {
 
         final String[][] expected = {
                 { "HIGH" }, // 10
-                { "LOW" },  // 0
-                { "LOW" }   // -5
+                { "LOW"  },  // 0
+                { "LOW"  }   // -5
         };
 
         assertSqlQueryRows("Bad apply UPPER to CASE...", expected, sql);
     }
 
     /**
-     * #3981
+     * #3852
      */
     @Test
-    public void testAggregateCaseOperatorAsColumn() {
+    public void testOrderByCaseOperator() {
         final String sql = String.format(
-                "SELECT (CASE WHEN value > 0 THEN 100 ELSE 10 END) AS 't', SUM(t)%n" +
-                "FROM '%s'",
+                "SELECT value %n" +
+                "FROM '%s' %n" +
+                "ORDER BY CASE WHEN value >= 0 THEN 1 ELSE 0 END ASC",
                 DEFAULT_SERIES.getMetric()
         );
 
         final String[][] expected = {
-                { "120" }   // 100 * 1 + 10 * 2
+                { "-5" },
+                { "10" },
+                {  "0" }
         };
 
-        assertSqlQueryRows("Bad apply SUM to CASE... as separate column", expected, sql);
+        assertSqlQueryRows("Bad ascending order by CASE...", expected, sql);
+    }
+
+    /**
+     * #3852
+     */
+    @Test
+    public void testOrderByCaseOperatorAsColumn() {
+        final String sql = String.format(
+                "SELECT value, CASE WHEN value >= 0 THEN 1 ELSE 0 END as 'val_group'%n" +
+                "FROM '%s' %n" +
+                "ORDER BY 'val_group' ASC",
+                DEFAULT_SERIES.getMetric()
+        );
+
+        final String[][] expected = {
+                { "-5", "0" },
+                { "10", "1" },
+                { "0",  "1" }
+        };
+
+        assertSqlQueryRows("Bad ascending order by CASE...", expected, sql);
+    }
+
+    /**
+     * #3852
+     */
+    @Test
+    public void testOrderByCaseOperatorOfDifferentTypes() {
+        final String sql = String.format(
+                "SELECT value %n" +
+                "FROM '%s' %n" +
+                "ORDER BY CASE WHEN value >= 0 THEN 'top' ELSE 10 END ASC",
+                DEFAULT_SERIES.getMetric()
+        );
+
+        final String[][] expected = {
+                { "-5" },
+                { "10" },
+                {  "0" }
+        };
+
+        assertSqlQueryRows("Bad ascending order by CASE...", expected, sql);
+    }
+
+    /**
+     * #3852
+     */
+    @Test
+    public void testCaseOperatorInWhereSwitchingCondition() {
+        final String sql = String.format(
+                "SELECT value %n" +
+                "FROM '%s' %n" +
+                "WHERE CASE WHEN value >= 0 THEN value > 5 ELSE value < -3 END",
+                DEFAULT_SERIES.getMetric()
+        );
+
+        final String[][] expected = {
+                { "10" },
+                { "-5" }
+        };
+
+        assertSqlQueryRows("Bad WHERE selection using boolean-type CASE...", expected, sql);
+    }
+
+
+    /**
+     * #3852
+     */
+    @Test
+    public void testCaseOperatorInWhere() {
+        final String sql = String.format(
+                "SELECT value %n" +
+                "FROM '%s' %n" +
+                "WHERE value > CASE WHEN value > -1 THEN 5 ELSE  -8 END",
+                DEFAULT_SERIES.getMetric()
+        );
+
+        final String[][] expected = {
+                { "10" },
+                { "-5" }
+        };
+
+        assertSqlQueryRows("Bad WHERE selection using boolean-type CASE...", expected, sql);
     }
 }
