338f19ba5c8f8a4c41588fa82ca3168293a366ec
Valery Tolstov <me@vtolstov.org>
2017-04-18 11:38:04 +0300

    Some rewrites for old code

--- src/test/java/com/axibase/tsd/api/method/sql/function/period/align/SqlPeriodDayAlignTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/function/period/align/SqlPeriodDayAlignTest.java
@@ -4,7 +4,6 @@ import com.axibase.tsd.api.method.series.SeriesMethod;
 import com.axibase.tsd.api.method.sql.SqlTest;
 import com.axibase.tsd.api.model.series.Sample;
 import com.axibase.tsd.api.model.series.Series;
-import com.axibase.tsd.api.model.sql.StringTable;
 import com.axibase.tsd.api.util.TestUtil;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -12,30 +11,27 @@ import org.testng.annotations.Test;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-import static com.axibase.tsd.api.util.TestUtil.ISOFormat;
-import static com.axibase.tsd.api.util.TestUtil.TestNames.entity;
-import static com.axibase.tsd.api.util.TestUtil.TestNames.metric;
-import static com.axibase.tsd.api.util.TestUtil.parseDate;
+import static com.axibase.tsd.api.util.TestUtil.TestNames;
 
 public class SqlPeriodDayAlignTest extends SqlTest {
-    private static final String TEST_METRIC_NAME = metric();
+    private static final String TEST_METRIC_NAME = TestNames.metric();
     private static final String DAY_FORMAT_PATTERN = "yyyy-MM-dd";
     private static final String START_TIME = "2016-06-19T00:00:00.000Z";
     private static final String END_TIME = "2016-06-23T00:00:00.000Z";
     private static final String ISO_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.ssS'Z'";
-    private static final Long DELTA = 900000L;
-    private static final Long DAY_LENGTH = 86400000L;
+    private static final long DELTA = 900000L;
+    private static final long DAY_LENGTH = 86400000L;
 
     @BeforeClass
     public static void prepareData() throws Exception {
-        Series series = new Series(entity(), TEST_METRIC_NAME);
-        Long firstTime = parseDate(START_TIME).getTime(),
-                lastTime = parseDate(END_TIME).getTime(),
-                time = firstTime;
-        while (time < lastTime) {
-            series.addData(new Sample(ISOFormat(time), "0"));
-            time += DELTA;
+        Series series = new Series(TestNames.entity(), TEST_METRIC_NAME);
+
+        long firstTime = TestUtil.parseDate(START_TIME).getTime();
+        long lastTime = TestUtil.parseDate(END_TIME).getTime();
+        for (long time = firstTime; time < lastTime; time += DELTA) {
+            series.addData(new Sample(TestUtil.ISOFormat(time), "0"));
         }
+
         SeriesMethod.insertSeriesCheck(Collections.singletonList(series));
     }
 
@@ -51,11 +47,7 @@ public class SqlPeriodDayAlignTest extends SqlTest {
                 DAY_FORMAT_PATTERN, TEST_METRIC_NAME
         );
 
-        StringTable resultTable = queryTable(sqlQuery);
-
-        List<List<String>> expectedRows = generateExpectedRows(null);
-
-        assertTableRowsExist(expectedRows, resultTable);
+        assertSqlQueryRows(generateExpectedRows(null), sqlQuery);
     }
 
     /**
@@ -73,11 +65,7 @@ public class SqlPeriodDayAlignTest extends SqlTest {
                 timeZone.getID()
         );
 
-        StringTable resultTable = queryTable(sqlQuery);
-
-        List<List<String>> expectedRows = generateExpectedRows(timeZone);
-
-        assertTableRowsExist(expectedRows, resultTable);
+        assertSqlQueryRows(generateExpectedRows(timeZone), sqlQuery);
     }
 
 
@@ -93,26 +81,27 @@ public class SqlPeriodDayAlignTest extends SqlTest {
             localEndDate = TestUtil.formatDate(TestUtil.parseDate(END_TIME), ISO_PATTERN, timeZone);
         }
 
-        Long startTime = TestUtil.parseDate(localStartDate).getTime();
-        Long endTime = TestUtil.parseDate(localEndDate).getTime();
-        Long time = startTime;
+        long startTime = TestUtil.parseDate(localStartDate).getTime();
+        long endTime = TestUtil.parseDate(localEndDate).getTime();
+        long time;
 
         int daySeriesCount = 0;
-        while (time < endTime) {
+        for (time = startTime; time < endTime; time += DELTA) {
             if (isDayStart(time) && daySeriesCount > 0) {
                 resultRows.add(formatRow(time - DAY_LENGTH, daySeriesCount));
                 daySeriesCount = 0;
             }
-            time += DELTA;
             daySeriesCount++;
         }
+
         if (daySeriesCount > 0) {
             resultRows.add(formatRow(time - DELTA, daySeriesCount));
         }
+
         return resultRows;
     }
 
-    private List<String> formatRow(Long time, Integer count) {
+    private List<String> formatRow(long time, int count) {
         SimpleDateFormat dateFormat = new SimpleDateFormat(DAY_FORMAT_PATTERN);
         dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
         return Arrays.asList(dateFormat.format(new Date(time)), Integer.toString(count));
