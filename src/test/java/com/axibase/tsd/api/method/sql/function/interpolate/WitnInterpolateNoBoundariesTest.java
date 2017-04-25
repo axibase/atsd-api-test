c7485dfdf7a3bacc0c9185bd6d8f9c976024046b
Valery Tolstov <me@vtolstov.org>
2017-04-25 13:13:40 +0300

    Test for #4120

---
+++ src/test/java/com/axibase/tsd/api/method/sql/function/interpolate/WitnInterpolateNoBoundariesTest.java
@@ -0,0 +1,41 @@
+package com.axibase.tsd.api.method.sql.function.interpolate;
+
+import com.axibase.tsd.api.method.series.SeriesMethod;
+import com.axibase.tsd.api.method.sql.SqlTest;
+import com.axibase.tsd.api.model.series.Series;
+import com.axibase.tsd.api.util.Mocks;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class WitnInterpolateNoBoundariesTest extends SqlTest {
+    private static String metricName;
+
+    @BeforeClass
+    public static void prepareData() throws Exception {
+        Series series = Mocks.series();
+        metricName = series.getMetric();
+
+        SeriesMethod.insertSeriesCheck(series);
+    }
+
+    /**
+     * #4120
+     */
+    @Test
+    public void testWitnInterpolateNoBoundaries() {
+        String sqlQuery = String.format(
+                "SELECT count(*) " +
+                        "FROM '%s' " +
+                        "WITH INTERPOLATE(1 MINUTE, PREVIOUS) " +
+                        "GROUP BY entity",
+                metricName
+        );
+
+        String[][] expectedRows = {
+                {"2"}
+        };
+
+        assertSqlQueryRows("Incorrect result for WITH INTERPOLATE without data boundaries",
+                expectedRows, sqlQuery);
+    }
+}
