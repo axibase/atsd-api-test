3aede5d67a4c745c60e1fa3dc068bf4a6efad7d3
Valery Tolstov <me@vtolstov.org>
2017-05-10 15:59:01 +0300

    Rewrite incomplete test

--- src/test/java/com/axibase/tsd/api/method/sql/function/period/filtering/SqlPeriodDataFilteringTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/function/period/filtering/SqlPeriodDataFilteringTest.java
@@ -1,88 +1,110 @@
 package com.axibase.tsd.api.method.sql.function.period.filtering;
 
 import com.axibase.tsd.api.method.series.SeriesMethod;
-import com.axibase.tsd.api.method.sql.SqlMethod;
+import com.axibase.tsd.api.method.sql.SqlTest;
 import com.axibase.tsd.api.model.series.Sample;
 import com.axibase.tsd.api.model.series.Series;
-import com.axibase.tsd.api.model.sql.StringTable;
+import com.axibase.tsd.api.util.TestUtil.*;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
-import static org.testng.AssertJUnit.assertEquals;
-
-
-public class SqlPeriodDataFilteringTest extends SqlMethod {
-    private static final String TEST_PREFIX = "sql-period-data-filtering";
-    private static final String TEST_METRIC_NAME = TEST_PREFIX + "metric";
-    private static final String TEST_ENTITY_NAME = TEST_PREFIX + "entity";
+public class SqlPeriodDataFilteringTest extends SqlTest {
+    private static final String TEST_METRIC_NAME = TestNames.metric();
+    private static final String TEST_ENTITY_NAME = TestNames.entity();
 
 
     @BeforeClass
     public static void prepareDataSet() throws Exception {
-        Series series = new Series(TEST_ENTITY_NAME, TEST_METRIC_NAME) {{
-            setSamples(Arrays.asList(
-                    new Sample("2016-06-27T14:20:00.000Z", "4.0"),
-                    new Sample("2016-06-27T14:22:00.000Z", "4.0"),
-                    new Sample("2016-06-27T14:22:01.000Z", "4.0")
-            ));
-        }};
-        SeriesMethod.insertSeriesCheck(Collections.singletonList(series));
-    }
+        Series series = new Series(TEST_ENTITY_NAME, TEST_METRIC_NAME);
 
-
-    /*
-      #2967
-      period filtering different from series query API
-     */
-
-    /**
-     * #2967
-     */
-    @Test
-    public void testFirstValueOutOfBounds() {
-        final String sqlQuery = String.format(
-                "SELECT datetime, count(value) FROM '%s' %nWHERE entity = '%s' " +
-                        "AND datetime >= '2016-06-27T14:20:01Z' and datetime < '2016-06-27T14:21:01Z' %n" +
-                        "GROUP BY PERIOD(1 MINUTE)",
-                TEST_METRIC_NAME, TEST_ENTITY_NAME
-        );
-
-        final List<List<String>> resultTableRows =
-                queryResponse(sqlQuery)
-                        .readEntity(StringTable.class)
-                        .getRows();
-
-        final List<List<String>> expectedTableRows = new ArrayList<>();
-        /*
-        Expected empty set, because period begin out of [start time; end time]
-         */
-        assertEquals(expectedTableRows, resultTableRows);
+        series.addData(new Sample("2017-01-01T00:00:00.000Z", 1));
+        series.addData(new Sample("2017-01-01T00:01:00.000Z", 2));
+        series.addData(new Sample("2017-01-01T01:00:00.000Z", 3));
+        series.addData(new Sample("2017-01-01T02:00:00.000Z", 4));
+        series.addData(new Sample("2017-01-01T02:01:00.000Z", 5));
+        SeriesMethod.insertSeriesCheck(series);
     }
 
     /**
-     * #2967
+     * #2967, #4146
      */
     @Test
-    public void testLastValueOutOfBounds() {
+    public void testPeriodFilterLeftBound() {
         final String sqlQuery = String.format(
-                "SELECT datetime, count(value) FROM '%s' %nWHERE entity = '%s' %n" +
-                        "AND datetime >= '2016-06-27T14:21:00Z' and datetime < '2016-06-27T14:22:01Z' %n" +
-                        "GROUP BY PERIOD(1 MINUTE)",
-                TEST_METRIC_NAME, TEST_ENTITY_NAME
+                "SELECT datetime, sum(value), count(value) FROM '%s' " +
+                        "WHERE datetime > '2017-01-01T00:00:00.000Z'" +
+                        "GROUP BY PERIOD(1 HOUR)",
+                TEST_METRIC_NAME
         );
 
-        final List<List<String>> resultTableRows =
-                queryResponse(sqlQuery)
-                        .readEntity(StringTable.class)
-                        .getRows();
-        final List<List<String>> expectedTableRows = Collections.singletonList(
-                Arrays.asList("2016-06-27T14:22:00.000Z", "1")
+        String[][] expectedRows = {
+                {"2017-01-01T00:00:00.000Z", "2", "1"},
+                {"2017-01-01T01:00:00.000Z", "3", "1"},
+                {"2017-01-01T02:00:00.000Z", "9", "2"},
+        };
+
+        assertSqlQueryRows("Wrong result if period is not in range (left bound)", expectedRows, sqlQuery);
+    }
+
+    /**
+     * #2967, #4146
+     */
+    @Test
+    public void testPeriodFilterRightBound() {
+        final String sqlQuery = String.format(
+                "SELECT datetime, sum(value), count(value) FROM '%s' " +
+                        "WHERE datetime <= '2017-01-01T02:00:00.000Z'" +
+                        "GROUP BY PERIOD(1 HOUR)",
+                TEST_METRIC_NAME
         );
-        assertEquals(expectedTableRows, resultTableRows);
+
+        String[][] expectedRows = {
+                {"2017-01-01T00:00:00.000Z", "3", "2"},
+                {"2017-01-01T01:00:00.000Z", "3", "1"},
+                {"2017-01-01T02:00:00.000Z", "4", "1"},
+        };
+
+        assertSqlQueryRows("Wrong result if period is not in range (right bound)", expectedRows, sqlQuery);
+    }
+
+    /**
+     * #2967, #4146
+     */
+    @Test
+    public void testPeriodFilterBothBounds() {
+        final String sqlQuery = String.format(
+                "SELECT datetime, sum(value), count(value) FROM '%s' " +
+                        "WHERE datetime > '2017-01-01T00:00:00.000Z' AND datetime <= '2017-01-01T02:00:00.000Z' " +
+                        "GROUP BY PERIOD(1 HOUR)",
+                TEST_METRIC_NAME
+        );
+
+        String[][] expectedRows = {
+                {"2017-01-01T00:00:00.000Z", "2", "1"},
+                {"2017-01-01T01:00:00.000Z", "3", "1"},
+                {"2017-01-01T02:00:00.000Z", "4", "1"},
+        };
+        assertSqlQueryRows("Wrong result if period is not in range (both bounds)", expectedRows, sqlQuery);
+    }
+
+    /**
+     * #2967, #4146
+     */
+    @Test
+    public void testPeriodNonFilterBothBounds() {
+        final String sqlQuery = String.format(
+                "SELECT datetime, sum(value), count(value) FROM '%s' " +
+                        "WHERE datetime >= '2017-01-01T00:00:00.000Z' AND datetime <= '2017-01-01T02:01:00.000Z' " +
+                        "GROUP BY PERIOD(1 HOUR)",
+                TEST_METRIC_NAME
+        );
+
+        String[][] expectedRows = {
+                {"2017-01-01T00:00:00.000Z", "3", "2"},
+                {"2017-01-01T01:00:00.000Z", "3", "1"},
+                {"2017-01-01T02:00:00.000Z", "9", "2"},
+        };
+        assertSqlQueryRows("Wrong result if all periods are in range", expectedRows, sqlQuery);
     }
 }
