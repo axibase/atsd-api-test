e907da4c720d90bceb75bd2125b614d860aa2fcb
alexey-kravtsov <Havok.08@mail.ru>
2017-05-23 10:54:07 +0300

    Added test for #4181

--- src/test/java/com/axibase/tsd/api/method/sql/function/interpolate/InterpolationBoundaryValuesTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/function/interpolate/InterpolationBoundaryValuesTest.java
@@ -6,16 +6,18 @@ import com.axibase.tsd.api.method.version.VersionMethod;
 import com.axibase.tsd.api.model.series.Sample;
 import com.axibase.tsd.api.model.series.Series;
 import com.axibase.tsd.api.model.version.Version;
+import com.axibase.tsd.api.util.Registry;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import javax.ws.rs.core.Response;
 
-import static com.axibase.tsd.api.util.TestUtil.TestNames.entity;
 import static com.axibase.tsd.api.util.TestUtil.TestNames.metric;
+import static com.axibase.tsd.api.util.TestUtil.TestNames.entity;
 
 public class InterpolationBoundaryValuesTest extends SqlTest {
-    private static final String TEST_METRIC = metric();
+    private static final String TEST_METRIC_1 = metric();
+    private static final String TEST_METRIC_2 = metric();
     private final String serverTimezoneOffset;
 
     // It is necessary to insert values with server timezone because interpolation works only by server local time
@@ -34,19 +36,36 @@ public class InterpolationBoundaryValuesTest extends SqlTest {
     @BeforeClass
     public void prepareData() throws Exception {
         String entity = entity();
+        Registry.Entity.register(entity);
+        
+        Registry.Metric.register(TEST_METRIC_1);
+        Registry.Metric.register(TEST_METRIC_2);
 
-        Series series = new Series(entity, TEST_METRIC);
+        Series series1 = new Series();
+        series1.setEntity(entity);
+        series1.setMetric(TEST_METRIC_1);
 
-        series.addSamples(new Sample(replaceTimezone("2017-01-01T07:30:00.000Z"), 0));
-        series.addSamples(new Sample(replaceTimezone("2017-01-01T10:30:00.000Z"), 1));
-        series.addSamples(new Sample(replaceTimezone("2017-01-01T11:30:00.000Z"), 2));
-        series.addSamples(new Sample(replaceTimezone("2017-01-01T12:30:00.000Z"), 3));
+        series1.addData(new Sample("1970-01-01T00:00:00Z", 0));
+        series1.addData(new Sample("1972-01-01T00:00:00Z", 2));
+        series1.addData(new Sample("1974-01-01T00:00:00Z", 4));
 
-        series.addSamples(new Sample(replaceTimezone("2017-01-01T17:30:00.000Z"), 7));
-        series.addSamples(new Sample(replaceTimezone("2017-01-01T18:30:00.000Z"), 8));
-        series.addSamples(new Sample(replaceTimezone("2017-01-01T19:30:00.000Z"), 9));
+        series1.addData(new Sample(replaceTimezone("2017-01-01T07:30:00.000Z"), 0));
+        series1.addData(new Sample(replaceTimezone("2017-01-01T10:30:00.000Z"), 1));
+        series1.addData(new Sample(replaceTimezone("2017-01-01T11:30:00.000Z"), 2));
+        series1.addData(new Sample(replaceTimezone("2017-01-01T12:30:00.000Z"), 3));
 
-        SeriesMethod.insertSeriesCheck(series);
+        series1.addData(new Sample(replaceTimezone("2017-01-01T17:30:00.000Z"), 7));
+        series1.addData(new Sample(replaceTimezone("2017-01-01T18:30:00.000Z"), 8));
+        series1.addData(new Sample(replaceTimezone("2017-01-01T19:30:00.000Z"), 9));
+
+        Series series2 = new Series();
+        series2.setEntity(entity);
+        series2.setMetric(TEST_METRIC_2);
+
+        series2.addData(new Sample("1971-01-01T00:00:00Z", 1));
+        series2.addData(new Sample("1973-01-01T00:00:00Z", 3));
+
+        SeriesMethod.insertSeriesCheck(series1, series2);
     }
 
     /**
@@ -62,7 +81,7 @@ public class InterpolationBoundaryValuesTest extends SqlTest {
                     "      OR datetime BETWEEN '2017-01-01T16:00:00Z' AND '2017-01-01T21:00:00Z' " +
                     "WITH INTERPOLATE(1 HOUR, PREVIOUS, INNER, NAN) " +
                     "ORDER BY datetime"),
-                TEST_METRIC);
+                TEST_METRIC_1);
 
         String[][] expectedRows = {
                 {"NaN"},
@@ -95,7 +114,7 @@ public class InterpolationBoundaryValuesTest extends SqlTest {
                      "     OR datetime BETWEEN '2017-01-01T16:00:00Z' AND '2017-01-01T21:00:00Z') " +
                      "WITH INTERPOLATE(1 HOUR, PREVIOUS, INNER, NAN) " +
                      "ORDER BY datetime"),
-                TEST_METRIC);
+                TEST_METRIC_1);
 
         String[][] expectedRows = {
                 {"NaN"},
@@ -127,12 +146,12 @@ public class InterpolationBoundaryValuesTest extends SqlTest {
                     "      OR datetime BETWEEN '2017-01-01T18:00:00Z' AND '2017-01-01T21:00:00Z' " +
                     "WITH INTERPOLATE(1 HOUR, PREVIOUS, INNER, NAN) " +
                     "ORDER BY datetime"),
-                TEST_METRIC);
+                TEST_METRIC_1);
 
         String[][] expectedRows = {
                 {"NaN"},
                 {"3"},
-                {"NaN"},
+                {"7"},
                 {"8"},
                 {"9"},
                 {"9"}
@@ -157,7 +176,7 @@ public class InterpolationBoundaryValuesTest extends SqlTest {
                         "      OR datetime BETWEEN '2017-01-01T22:00:00Z' AND '2017-01-01T23:00:00Z' " +
                         "WITH INTERPOLATE(1 HOUR, PREVIOUS, INNER, NAN) " +
                         "ORDER BY datetime"),
-                TEST_METRIC);
+                TEST_METRIC_1);
 
         String[][] expectedRows = {
                 {"NaN"},
@@ -184,7 +203,7 @@ public class InterpolationBoundaryValuesTest extends SqlTest {
                         "      OR datetime BETWEEN '2017-01-01T16:00:00Z' AND '2017-01-01T21:00:00Z' " +
                         "WITH INTERPOLATE(1 HOUR, PREVIOUS, OUTER, NAN) " +
                         "ORDER BY datetime"),
-                TEST_METRIC);
+                TEST_METRIC_1);
 
         String[][] expectedRows = {
                 {"NaN"},
@@ -215,7 +234,7 @@ public class InterpolationBoundaryValuesTest extends SqlTest {
                     "      OR datetime BETWEEN '2017-01-01T16:00:00Z' AND '2017-01-01T21:00:00Z' " +
                     "WITH INTERPOLATE(1 HOUR, PREVIOUS, OUTER, NAN) " +
                     "ORDER BY datetime"),
-                TEST_METRIC);
+                TEST_METRIC_1);
 
         String[][] expectedRows = {
                 {"NaN"},
@@ -247,7 +266,7 @@ public class InterpolationBoundaryValuesTest extends SqlTest {
                     "     OR datetime BETWEEN '2017-01-01T17:00:00Z' AND '2017-01-01T21:00:00Z') " +
                     "WITH INTERPOLATE(1 HOUR, PREVIOUS, OUTER, NAN) " +
                     "ORDER BY datetime"),
-                TEST_METRIC);
+                TEST_METRIC_1);
 
         String[][] expectedRows = {
                 {"NaN"},
@@ -278,7 +297,7 @@ public class InterpolationBoundaryValuesTest extends SqlTest {
                         "      OR datetime BETWEEN '2017-01-01T18:00:00Z' AND '2017-01-01T21:00:00Z' " +
                         "WITH INTERPOLATE(1 HOUR, PREVIOUS, OUTER, NAN) " +
                         "ORDER BY datetime"),
-                TEST_METRIC);
+                TEST_METRIC_1);
 
         String[][] expectedRows = {
                 {"2"},
@@ -307,7 +326,7 @@ public class InterpolationBoundaryValuesTest extends SqlTest {
                         "      OR datetime BETWEEN '2017-01-01T22:00:00Z' AND '2017-01-01T23:00:00Z' " +
                         "WITH INTERPOLATE(1 HOUR, PREVIOUS, OUTER, NAN) " +
                         "ORDER BY datetime"),
-                TEST_METRIC);
+                TEST_METRIC_1);
 
         String[][] expectedRows = {
                 {"7"},
@@ -333,7 +352,7 @@ public class InterpolationBoundaryValuesTest extends SqlTest {
                         "      OR datetime BETWEEN '2017-01-01T18:00:00Z' AND '2017-01-01T19:00:00Z' " +
                         "WITH INTERPOLATE(1 HOUR, PREVIOUS, OUTER, NAN) " +
                         "ORDER BY datetime"),
-                TEST_METRIC);
+                TEST_METRIC_1);
 
         String[][] expectedRows = {
                 {"3"},
@@ -361,7 +380,7 @@ public class InterpolationBoundaryValuesTest extends SqlTest {
                               "OR datetime BETWEEN '2017-01-01T12:00:00Z' AND '2017-01-01T14:00:00Z' " +
                         "WITH INTERPOLATE(1 HOUR, PREVIOUS, OUTER, NAN) " +
                         "ORDER BY datetime",
-                TEST_METRIC);
+                TEST_METRIC_1);
 
         Response response = queryResponse(sqlQuery);
 
@@ -372,5 +391,178 @@ public class InterpolationBoundaryValuesTest extends SqlTest {
 
         assertBadRequest("Incorrect overlapping time intervals error handling",
                 expectedErrorMessage, response);
+    }
+
+    /**
+     * #4181
+     */
+    @Test
+    public void testJoinWithMinDateNoneCalendar() {
+        String sqlQuery = String.format(
+                "SELECT m1.datetime, m1.value, m2.datetime, m2.value " +
+                        "FROM '%s' m1 " +
+                        "JOIN '%s' m2 " +
+                        "WHERE m1.datetime >= '1970-01-01T00:00:00Z' AND m1.datetime < '1975-01-01T00:00:00Z' " +
+                        "WITH INTERPOLATE(1 YEAR, PREVIOUS, INNER, NONE, CALENDAR)",
+                TEST_METRIC_1,
+                TEST_METRIC_2);
+
+        String[][] expectedRows = {
+                {"1971-01-01T00:00:00.000Z", "0", "1971-01-01T00:00:00.000Z", "1"},
+                {"1972-01-01T00:00:00.000Z", "2", "1972-01-01T00:00:00.000Z", "1"},
+                {"1973-01-01T00:00:00.000Z", "2", "1973-01-01T00:00:00.000Z", "3"},
+                {"1974-01-01T00:00:00.000Z", "4", "1974-01-01T00:00:00.000Z", "3"},
+        };
+
+        assertSqlQueryRows(expectedRows, sqlQuery);
+    }
+
+    /**
+     * #4181
+     */
+    @Test
+    public void testJoinWithMinDateNanCalendar() {
+        String sqlQuery = String.format(
+                "SELECT m1.datetime, m1.value, m2.datetime, m2.value " +
+                        "FROM '%s' m1 " +
+                        "JOIN '%s' m2 " +
+                        "WHERE m1.datetime >= '1970-01-01T00:00:00Z' AND m1.datetime < '1975-01-01T00:00:00Z' " +
+                        "WITH INTERPOLATE(1 YEAR, PREVIOUS, INNER, NAN, CALENDAR)",
+                TEST_METRIC_1,
+                TEST_METRIC_2);
+
+        String[][] expectedRows = {
+                {"1970-01-01T00:00:00.000Z", "0", "1970-01-01T00:00:00.000Z", "NaN"},
+                {"1971-01-01T00:00:00.000Z", "0", "1971-01-01T00:00:00.000Z", "1"},
+                {"1972-01-01T00:00:00.000Z", "2", "1972-01-01T00:00:00.000Z", "1"},
+                {"1973-01-01T00:00:00.000Z", "2", "1973-01-01T00:00:00.000Z", "3"},
+                {"1974-01-01T00:00:00.000Z", "4", "1974-01-01T00:00:00.000Z", "3"},
+        };
+
+        assertSqlQueryRows(expectedRows, sqlQuery);
+    }
+
+    /**
+     * #4181
+     */
+    @Test
+    public void testJoinWithMinDateExtendCalendar() {
+        String sqlQuery = String.format(
+                "SELECT m1.datetime, m1.value, m2.datetime, m2.value " +
+                        "FROM '%s' m1 " +
+                        "JOIN '%s' m2 " +
+                        "WHERE m1.datetime >= '1970-01-01T00:00:00Z' AND m1.datetime < '1975-01-01T00:00:00Z' " +
+                        "WITH INTERPOLATE(1 YEAR, PREVIOUS, INNER, EXTEND, CALENDAR)",
+                TEST_METRIC_1,
+                TEST_METRIC_2);
+
+        String[][] expectedRows = {
+                {"1970-01-01T00:00:00.000Z", "0", "1970-01-01T00:00:00.000Z", "1"},
+                {"1971-01-01T00:00:00.000Z", "0", "1971-01-01T00:00:00.000Z", "1"},
+                {"1972-01-01T00:00:00.000Z", "2", "1972-01-01T00:00:00.000Z", "1"},
+                {"1973-01-01T00:00:00.000Z", "2", "1973-01-01T00:00:00.000Z", "3"},
+                {"1974-01-01T00:00:00.000Z", "4", "1974-01-01T00:00:00.000Z", "3"},
+        };
+
+        assertSqlQueryRows(expectedRows, sqlQuery);
+    }
+
+    /**
+     * #4181
+     */
+    @Test
+    public void testJoinWithMinDateNoneStartTime() {
+        String sqlQuery = String.format(
+                "SELECT m1.datetime, m1.value, m2.datetime, m2.value " +
+                        "FROM '%s' m1 " +
+                        "JOIN '%s' m2 " +
+                        "WHERE m1.datetime >= '1970-01-01T00:00:00Z' AND m1.datetime < '1975-01-01T00:00:00Z' " +
+                        "WITH INTERPOLATE(1 YEAR, PREVIOUS, INNER, NONE, START_TIME)",
+                TEST_METRIC_1,
+                TEST_METRIC_2);
+
+        String[][] expectedRows = {
+                {"1971-01-01T00:00:00.000Z", "0", "1971-01-01T00:00:00.000Z", "1"},
+                {"1972-01-01T00:00:00.000Z", "2", "1972-01-01T00:00:00.000Z", "1"},
+                {"1973-01-01T00:00:00.000Z", "2", "1973-01-01T00:00:00.000Z", "3"},
+                {"1974-01-01T00:00:00.000Z", "4", "1974-01-01T00:00:00.000Z", "3"},
+        };
+
+        assertSqlQueryRows(expectedRows, sqlQuery);
+    }
+
+    /**
+     * #4181
+     */
+    @Test
+    public void testJoinWithMinDateNanStartTime() {
+        String sqlQuery = String.format(
+                "SELECT m1.datetime, m1.value, m2.datetime, m2.value " +
+                        "FROM '%s' m1 " +
+                        "JOIN '%s' m2 " +
+                        "WHERE m1.datetime >= '1970-01-01T00:00:00Z' AND m1.datetime < '1975-01-01T00:00:00Z' " +
+                        "WITH INTERPOLATE(1 YEAR, PREVIOUS, INNER, NAN, START_TIME)",
+                TEST_METRIC_1,
+                TEST_METRIC_2);
+
+        String[][] expectedRows = {
+                {"1970-01-01T00:00:00.000Z", "0", "1970-01-01T00:00:00.000Z", "NaN"},
+                {"1971-01-01T00:00:00.000Z", "0", "1971-01-01T00:00:00.000Z", "1"},
+                {"1972-01-01T00:00:00.000Z", "2", "1972-01-01T00:00:00.000Z", "1"},
+                {"1973-01-01T00:00:00.000Z", "2", "1973-01-01T00:00:00.000Z", "3"},
+                {"1974-01-01T00:00:00.000Z", "4", "1974-01-01T00:00:00.000Z", "3"},
+        };
+
+        assertSqlQueryRows(expectedRows, sqlQuery);
+    }
+
+    /**
+     * #4181
+     */
+    @Test
+    public void testJoinWithMinDateExtendStartTime() {
+        String sqlQuery = String.format(
+                "SELECT m1.datetime, m1.value, m2.datetime, m2.value " +
+                        "FROM '%s' m1 " +
+                        "JOIN '%s' m2 " +
+                        "WHERE m1.datetime >= '1970-01-01T00:00:00Z' AND m1.datetime < '1975-01-01T00:00:00Z' " +
+                        "WITH INTERPOLATE(1 YEAR, PREVIOUS, INNER, EXTEND, START_TIME)",
+                TEST_METRIC_1,
+                TEST_METRIC_2);
+
+        String[][] expectedRows = {
+                {"1970-01-01T00:00:00.000Z", "0", "1970-01-01T00:00:00.000Z", "1"},
+                {"1971-01-01T00:00:00.000Z", "0", "1971-01-01T00:00:00.000Z", "1"},
+                {"1972-01-01T00:00:00.000Z", "2", "1972-01-01T00:00:00.000Z", "1"},
+                {"1973-01-01T00:00:00.000Z", "2", "1973-01-01T00:00:00.000Z", "3"},
+                {"1974-01-01T00:00:00.000Z", "4", "1974-01-01T00:00:00.000Z", "3"},
+        };
+
+        assertSqlQueryRows(expectedRows, sqlQuery);
+    }
+
+    /**
+     * #4181
+     */
+    @Test
+    public void testJoinWithDateBeforeMin() {
+        String sqlQuery = String.format(
+                "SELECT m1.datetime, m1.value, m2.datetime, m2.value " +
+                        "FROM '%s' m1 " +
+                        "JOIN '%s' m2 " +
+                        "WHERE m1.datetime >= '1969-01-01T00:00:00Z' AND m1.datetime < '1975-01-01T00:00:00Z' " +
+                        "WITH INTERPOLATE(1 YEAR, PREVIOUS, INNER, EXTEND, START_TIME)",
+                TEST_METRIC_1,
+                TEST_METRIC_2);
+
+        String[][] expectedRows = {
+                {"1970-01-01T00:00:00.000Z", "0", "1970-01-01T00:00:00.000Z", "1"},
+                {"1971-01-01T00:00:00.000Z", "0", "1971-01-01T00:00:00.000Z", "1"},
+                {"1972-01-01T00:00:00.000Z", "2", "1972-01-01T00:00:00.000Z", "1"},
+                {"1973-01-01T00:00:00.000Z", "2", "1973-01-01T00:00:00.000Z", "3"},
+                {"1974-01-01T00:00:00.000Z", "4", "1974-01-01T00:00:00.000Z", "3"},
+        };
+
+        assertSqlQueryRows(expectedRows, sqlQuery);
     }
 }
