fc335cdb001276855464180a17127d268af41b7c
Valery Tolstov <me@vtolstov.org>
2017-05-25 11:56:05 +0300

    Use new constructor

--- src/test/java/com/axibase/tsd/api/method/sql/clause/join/JoinWithAtsdSeriesTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/clause/join/JoinWithAtsdSeriesTest.java
@@ -17,36 +17,25 @@ public class JoinWithAtsdSeriesTest extends SqlTest {
     public static void prepareData() throws Exception {
         String entityName = TestNames.entity();
 
-        Registry.Entity.register(entityName);
+        Series series1 = new Series(entityName, METRIC_NAME1);
+        series1.addData(
+                new Sample("2017-01-01T12:00:00.000Z", "1"),
+                new Sample("2017-01-02T12:00:00.000Z", "2"),
+                new Sample("2017-01-04T12:00:00.000Z", "4")
+        );
 
-        Registry.Metric.register(METRIC_NAME1);
-        Registry.Metric.register(METRIC_NAME2);
-
-        Series series1 = new Series();
-        series1.setEntity(entityName);
-        series1.setMetric(METRIC_NAME1);
-        series1.addData(new Sample("2017-01-01T12:00:00.000Z", "1"));
-        series1.addData(new Sample("2017-01-02T12:00:00.000Z", "2"));
-        series1.addData(new Sample("2017-01-04T12:00:00.000Z", "4"));
-
-        Series series2 = new Series();
-        series2.setEntity(entityName);
-        series2.setMetric(METRIC_NAME1);
+        Series series2 = new Series(entityName, METRIC_NAME1, "t1", "tag");
         series2.addData(new Sample("2017-01-03T12:00:00.000Z", "3"));
-        series2.addTag("t1", "tag");
 
-        Series series3 = new Series();
-        series3.setEntity(entityName);
-        series3.setMetric(METRIC_NAME2);
+        Series series3 = new Series(entityName, METRIC_NAME2, "t2", "tag");
         series3.addData(new Sample("2017-01-03T12:00:00.000Z", "5"));
-        series3.addTag("t2", "tag");
 
-        Series series4 = new Series();
-        series4.setEntity(entityName);
-        series4.setMetric(METRIC_NAME2);
-        series4.addData(new Sample("2017-01-04T12:00:00.000Z", "6"));
-        series4.addData(new Sample("2017-01-05T12:00:00.000Z", "7"));
-        series4.addData(new Sample("2017-01-06T12:00:00.000Z", "8"));
+        Series series4 = new Series(entityName, METRIC_NAME2);
+        series4.addData(
+                new Sample("2017-01-04T12:00:00.000Z", "6"),
+                new Sample("2017-01-05T12:00:00.000Z", "7"),
+                new Sample("2017-01-06T12:00:00.000Z", "8")
+        );
 
         SeriesMethod.insertSeriesCheck(series1, series2, series3, series4);
     }
@@ -111,7 +100,7 @@ public class JoinWithAtsdSeriesTest extends SqlTest {
         String[][] expectedRows = {
                 {   "1",    "null"},
                 {   "2",    "null"},
-                {   "3",       "5"},
+                {   "3",    "null"},
                 {"null",       "5"},
                 {   "4",       "6"},
                 {"null",       "7"},
