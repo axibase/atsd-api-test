44a52c62b4f2645d0a7c58d75492675d505cc6ee
Valery Tolstov <me@vtolstov.org>
2017-05-26 13:39:44 +0300

    Add more tests

--- src/test/java/com/axibase/tsd/api/method/sql/clause/join/JoinWithAtsdSeriesTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/clause/join/JoinWithAtsdSeriesTest.java
@@ -1,7 +1,9 @@
 package com.axibase.tsd.api.method.sql.clause.join;
 
+import com.axibase.tsd.api.method.metric.MetricMethod;
 import com.axibase.tsd.api.method.series.SeriesMethod;
 import com.axibase.tsd.api.method.sql.SqlTest;
+import com.axibase.tsd.api.model.metric.Metric;
 import com.axibase.tsd.api.model.series.Sample;
 import com.axibase.tsd.api.model.series.Series;
 import com.axibase.tsd.api.util.Registry;
@@ -12,6 +14,8 @@ import org.testng.annotations.Test;
 public class JoinWithAtsdSeriesTest extends SqlTest {
     private static final String METRIC_NAME1 = TestNames.metric();
     private static final String METRIC_NAME2 = TestNames.metric();
+    private static final String METRIC_NAME3 = TestNames.metric();
+    private static final String METRIC_NAME4 = TestNames.metric();
 
     @BeforeClass
     public static void prepareData() throws Exception {
@@ -37,7 +41,14 @@ public class JoinWithAtsdSeriesTest extends SqlTest {
                 new Sample("2017-01-06T12:00:00.000Z", "8")
         );
 
-        SeriesMethod.insertSeriesCheck(series1, series2, series3, series4);
+        Series series5 = series4.copy();
+        Registry.Metric.register(METRIC_NAME3);
+        series5.setMetric(METRIC_NAME3);
+
+        SeriesMethod.insertSeriesCheck(series1, series2, series3, series4, series5);
+
+        Registry.Metric.register(METRIC_NAME4);
+        MetricMethod.createOrReplaceMetricCheck(new Metric(METRIC_NAME4));
     }
 
     /**
@@ -67,6 +78,64 @@ public class JoinWithAtsdSeriesTest extends SqlTest {
         };
 
         assertSqlQueryRows("Wrong result for join from atsd_series", expectedRows, sqlQuery);
+    }
+
+    /**
+     * #4089
+     */
+    @Test
+    public void testJoinEmptyFromAtsdSeries() {
+        /*
+        SELECT t1.value, t2.value FROM atsd_series t1 JOIN m3 t2 WHERE t1.metric = m1
+
+        | t1.value | t2.value |
+        |----------|----------|
+        | 4        | 6        |
+         */
+
+        String sqlQuery= String.format(
+                "SELECT t1.value, t2.value " +
+                        "FROM atsd_series t1 " +
+                        "JOIN '%2$s' t2 " +
+                        "WHERE t1.metric = '%1$s'",
+                METRIC_NAME1,
+                METRIC_NAME4
+        );
+
+        String[][] expectedRows = {};
+
+        assertSqlQueryRows("Wrong result for empty join from atsd_series", expectedRows, sqlQuery);
+    }
+
+    /**
+     * #4089
+     */
+    @Test
+    public void testMultipleJoinFromAtsdSeries() {
+        /*
+        SELECT t1.value, t2.value, t3.value FROM atsd_series t1 JOIN m2 t2 JOIN m3 t3 WHERE t1.metric = m1
+
+        | t1.value | t2.value | t3.value |
+        |----------|----------|----------|
+        | 4        | 6        | 6        |
+         */
+
+        String sqlQuery= String.format(
+                "SELECT t1.value, t2.value, t3.value " +
+                        "FROM atsd_series t1 " +
+                        "JOIN '%2$s' t2 " +
+                        "JOIN '%3$s' t3 " +
+                        "WHERE t1.metric = '%1$s'",
+                METRIC_NAME1,
+                METRIC_NAME2,
+                METRIC_NAME3
+        );
+
+        String[][] expectedRows = {
+                {"4", "6", "6"}
+        };
+
+        assertSqlQueryRows("Wrong result for multiple join from atsd_series", expectedRows, sqlQuery);
     }
 
     /**
@@ -141,7 +210,81 @@ public class JoinWithAtsdSeriesTest extends SqlTest {
         assertSqlQueryRows("Wrong result for outer join from atsd_series", expectedRows, sqlQuery);
     }
 
+    /**
+     * #4089
+     */
+    @Test
+    public void testOuterJoinEmptyFromAtsdSeries() {
+        /*
+        SELECT t1.value, t2.value FROM atsd_series t1 OUTER JOIN m2 t2 WHERE t1.metric = m1
 
+        | t1.value | t2.value |
+        |----------|----------|
+        | 1        | null     |
+        | 2        | null     |
+        | 3        | null     |
+        | 4        | null     |
+         */
+
+        String sqlQuery= String.format(
+                "SELECT t1.value, t2.value " +
+                        "FROM atsd_series t1 " +
+                        "OUTER JOIN '%2$s' t2 " +
+                        "WHERE t1.metric = '%1$s'",
+                METRIC_NAME1,
+                METRIC_NAME3
+        );
+
+        String[][] expectedRows = {
+                {"1",    "null"},
+                {"2",    "null"},
+                {"3",    "null"},
+                {"4",    "null"},
+        };
+
+        assertSqlQueryRows("Wrong result for empty outer join from atsd_series", expectedRows, sqlQuery);
+    }
+
+    /**
+     * #4089
+     */
+    @Test
+    public void testMultipleOuterJoinFromAtsdSeries() {
+        /*
+        SELECT t1.value, t2.value FROM atsd_series t1 OUTER JOIN m2 t2 WHERE t1.metric = m1
+
+        | t1.value | t2.value | t3.value |
+        |----------|----------|----------|
+        | 1        | null     | null     |
+        | 2        | null     | null     |
+        | 3        | null     | null     |
+        | null     | 5        | null     |
+        | 4        | 6        | 6        |
+        | null     | 7        | 7        |
+        | null     | 8        | 8        |
+         */
+
+        String sqlQuery= String.format(
+                "SELECT t1.value, t2.value " +
+                        "FROM atsd_series t1 " +
+                        "OUTER JOIN '%2$s' t2 " +
+                        "WHERE t1.metric = '%1$s'",
+                METRIC_NAME1,
+                METRIC_NAME2
+        );
+
+        String[][] expectedRows = {
+                {   "1",    "null",    "null"},
+                {   "2",    "null",    "null"},
+                {   "3",    "null",    "null"},
+                {"null",       "5",    "null"},
+                {   "4",       "6",       "6"},
+                {"null",       "7",       "7"},
+                {"null",       "8",       "8"},
+        };
+
+        assertSqlQueryRows("Wrong result for multiple outer join from atsd_series", expectedRows, sqlQuery);
+    }
 
     /**
      * #4089
