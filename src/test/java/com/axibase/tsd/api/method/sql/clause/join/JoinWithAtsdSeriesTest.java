4ba8d5cd3ad9dfadae820a030cc5935829871aba
Valery Tolstov <me@vtolstov.org>
2017-05-29 15:23:51 +0300

    Use ORDER BY

--- src/test/java/com/axibase/tsd/api/method/sql/clause/join/JoinWithAtsdSeriesTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/clause/join/JoinWithAtsdSeriesTest.java
@@ -23,22 +23,22 @@ public class JoinWithAtsdSeriesTest extends SqlTest {
 
         Series series1 = new Series(entityName, METRIC_NAME1);
         series1.addSamples(
-                new Sample("2017-01-01T12:00:00.000Z", "1"),
-                new Sample("2017-01-02T12:00:00.000Z", "2"),
-                new Sample("2017-01-04T12:00:00.000Z", "4")
+                new Sample("2017-01-01T12:00:00.000Z", 1),
+                new Sample("2017-01-02T12:00:00.000Z", 2),
+                new Sample("2017-01-04T12:00:00.000Z", 4)
         );
 
         Series series2 = new Series(entityName, METRIC_NAME1, "t1", "tag");
-        series2.addSamples(new Sample("2017-01-03T12:00:00.000Z", "3"));
+        series2.addSamples(new Sample("2017-01-03T12:00:00.000Z", 3));
 
         Series series3 = new Series(entityName, METRIC_NAME2, "t2", "tag");
-        series3.addSamples(new Sample("2017-01-03T12:00:00.000Z", "5"));
+        series3.addSamples(new Sample("2017-01-03T12:00:00.000Z", 5));
 
         Series series4 = new Series(entityName, METRIC_NAME2);
         series4.addSamples(
-                new Sample("2017-01-04T12:00:00.000Z", "6"),
-                new Sample("2017-01-05T12:00:00.000Z", "7"),
-                new Sample("2017-01-06T12:00:00.000Z", "8")
+                new Sample("2017-01-04T12:00:00.000Z", 6),
+                new Sample("2017-01-05T12:00:00.000Z", 7),
+                new Sample("2017-01-06T12:00:00.000Z", 8)
         );
 
         Series series5 = series4.copy();
@@ -57,24 +57,26 @@ public class JoinWithAtsdSeriesTest extends SqlTest {
     @Test
     public void testJoinFromAtsdSeries() {
         /*
-        SELECT t1.value, t2.value FROM atsd_series t1 JOIN m2 t2 WHERE t1.metric = m1
+        SELECT t1.datetime, t1.value, t2.value FROM atsd_series t1 JOIN m2 t2 WHERE t1.metric = m1
+        ORDER BY t1.datetime
 
-        | t1.value | t2.value |
-        |----------|----------|
-        | 4        | 6        |
+        | t1.datetime              | t1.value | t2.value |
+        |--------------------------|----------|----------|
+        | 2017-01-04T12:00:00.000Z | 4        | 6        |
          */
 
-        String sqlQuery= String.format(
-                "SELECT t1.value, t2.value " +
+        String sqlQuery = String.format(
+                "SELECT t1.datetime, t1.value, t2.value " +
                         "FROM atsd_series t1 " +
                         "JOIN '%2$s' t2 " +
-                        "WHERE t1.metric = '%1$s'",
+                        "WHERE t1.metric = '%1$s' " +
+                        "ORDER BY t1.datetime",
                 METRIC_NAME1,
                 METRIC_NAME2
         );
 
         String[][] expectedRows = {
-                {"4", "6"}
+                {"2017-01-04T12:00:00.000Z", "4", "6"}
         };
 
         assertSqlQueryRows("Wrong result for join from atsd_series", expectedRows, sqlQuery);
@@ -86,18 +88,19 @@ public class JoinWithAtsdSeriesTest extends SqlTest {
     @Test
     public void testJoinEmptyFromAtsdSeries() {
         /*
-        SELECT t1.value, t2.value FROM atsd_series t1 JOIN m3 t2 WHERE t1.metric = m1
+        SELECT t1.datetime, t1.value, t2.value FROM atsd_series t1 JOIN m3 t2 WHERE t1.metric = m1
+        ORDER BY t1.datetime
 
-        | t1.value | t2.value |
-        |----------|----------|
-        | 4        | 6        |
+        | t1.datetime | t1.value | t2.value |
+        |-------------|----------|----------|
          */
 
-        String sqlQuery= String.format(
-                "SELECT t1.value, t2.value " +
+        String sqlQuery = String.format(
+                "SELECT t1.datetime, t1.value, t2.value " +
                         "FROM atsd_series t1 " +
                         "JOIN '%2$s' t2 " +
-                        "WHERE t1.metric = '%1$s'",
+                        "WHERE t1.metric = '%1$s' " +
+                        "ORDER BY t1.datetime",
                 METRIC_NAME1,
                 METRIC_NAME4
         );
@@ -114,25 +117,27 @@ public class JoinWithAtsdSeriesTest extends SqlTest {
     public void testMultipleJoinFromAtsdSeries() {
         /*
         SELECT t1.value, t2.value, t3.value FROM atsd_series t1 JOIN m2 t2 JOIN m3 t3 WHERE t1.metric = m1
+        ORDER BY t1.datetime
 
-        | t1.value | t2.value | t3.value |
-        |----------|----------|----------|
-        | 4        | 6        | 6        |
+        | t1.datetime              | t1.value | t2.value | t3.value |
+        |--------------------------|----------|----------|----------|
+        | 2017-01-04T12:00:00.000Z | 4        | 6        | 6        |
          */
 
-        String sqlQuery= String.format(
+        String sqlQuery = String.format(
                 "SELECT t1.value, t2.value, t3.value " +
                         "FROM atsd_series t1 " +
                         "JOIN '%2$s' t2 " +
                         "JOIN '%3$s' t3 " +
-                        "WHERE t1.metric = '%1$s'",
+                        "WHERE t1.metric = '%1$s' " +
+                        "ORDER BY t1.datetime",
                 METRIC_NAME1,
                 METRIC_NAME2,
                 METRIC_NAME3
         );
 
         String[][] expectedRows = {
-                {"4", "6", "6"}
+                {"2017-01-04T12:00:00.000Z", "4", "6", "6"}
         };
 
         assertSqlQueryRows("Wrong result for multiple join from atsd_series", expectedRows, sqlQuery);
@@ -144,26 +149,28 @@ public class JoinWithAtsdSeriesTest extends SqlTest {
     @Test
     public void testJoinFromAtsdSeriesUsingEntity() {
         /*
-        SELECT t1.value, t2.value FROM atsd_series t1 JOIN USING ENTITY m2 t2 WHERE t1.metric = m1
+        SELECT t1.datetime, t1.value, t2.value FROM atsd_series t1 JOIN USING ENTITY m2 t2 WHERE t1.metric = m1
+        ORDER BY t1.datetime
 
-        | t1.value | t2.value |
-        |----------|----------|
-        | 3        | 5        |
-        | 4        | 6        |
+        | t1.datetime              | t1.value | t2.value |
+        |--------------------------|----------|----------|
+        | 2017-01-03T12:00:00.000Z | 3        | 5        |
+        | 2017-01-04T12:00:00.000Z | 4        | 6        |
          */
 
-        String sqlQuery= String.format(
-                "SELECT t1.value, t2.value " +
+        String sqlQuery = String.format(
+                "SELECT t1.datetime, t1.value, t2.value " +
                         "FROM atsd_series t1 " +
                         "JOIN USING ENTITY '%2$s' t2 " +
-                        "WHERE t1.metric = '%1$s'",
+                        "WHERE t1.metric = '%1$s' " +
+                        "ORDER BY t1.datetime",
                 METRIC_NAME1,
                 METRIC_NAME2
         );
 
         String[][] expectedRows = {
-                {"3", "5"},
-                {"4", "6"}
+                {"2017-01-03T12:00:00.000Z", "3", "5"},
+                {"2017-01-04T12:00:00.000Z", "4", "6"}
         };
 
         assertSqlQueryRows("Wrong result for join using entity from atsd_series", expectedRows, sqlQuery);
@@ -175,36 +182,38 @@ public class JoinWithAtsdSeriesTest extends SqlTest {
     @Test
     public void testOuterJoinFromAtsdSeries() {
         /*
-        SELECT t1.value, t2.value FROM atsd_series t1 OUTER JOIN m2 t2 WHERE t1.metric = m1
+        SELECT t1.datetime, t1.value, t2.value FROM atsd_series t1 OUTER JOIN m2 t2 WHERE t1.metric = m1
+        ORDER BY t1.datetime
 
-        | t1.value | t2.value |
-        |----------|----------|
-        | 1        | null     |
-        | 2        | null     |
-        | 3        | null     |
-        | null     | 5        |
-        | 4        | 6        |
-        | null     | 7        |
-        | null     | 8        |
+        | t1.datetime              | t1.value | t2.value |
+        |--------------------------|----------|----------|
+        | 2017-01-01T12:00:00.000Z | 1        | null     |
+        | 2017-01-02T12:00:00.000Z | 2        | null     |
+        | 2017-01-03T12:00:00.000Z | 3        | null     |
+        | 2017-01-03T12:00:00.000Z | null     | 5        |
+        | 2017-01-04T12:00:00.000Z | 4        | 6        |
+        | 2017-01-05T12:00:00.000Z | null     | 7        |
+        | 2017-01-06T12:00:00.000Z | null     | 8        |
          */
 
-        String sqlQuery= String.format(
-                "SELECT t1.value, t2.value " +
+        String sqlQuery = String.format(
+                "SELECT t1.datetime, t1.value, t2.value " +
                         "FROM atsd_series t1 " +
                         "OUTER JOIN '%2$s' t2 " +
-                        "WHERE t1.metric = '%1$s'",
+                        "WHERE t1.metric = '%1$s' " +
+                        "ORDER BY t1.datetime",
                 METRIC_NAME1,
                 METRIC_NAME2
         );
 
         String[][] expectedRows = {
-                {   "1",    "null"},
-                {   "2",    "null"},
-                {   "3",    "null"},
-                {"null",       "5"},
-                {   "4",       "6"},
-                {"null",       "7"},
-                {"null",       "8"},
+                {"2017-01-01T12:00:00.000Z",    "1", "null"},
+                {"2017-01-02T12:00:00.000Z",    "2", "null"},
+                {"2017-01-03T12:00:00.000Z",    "3", "null"},
+                {"2017-01-03T12:00:00.000Z", "null",    "5"},
+                {"2017-01-04T12:00:00.000Z",    "4",    "6"},
+                {"2017-01-05T12:00:00.000Z", "null",    "7"},
+                {"2017-01-06T12:00:00.000Z", "null",    "8"},
         };
 
         assertSqlQueryRows("Wrong result for outer join from atsd_series", expectedRows, sqlQuery);
@@ -216,30 +225,32 @@ public class JoinWithAtsdSeriesTest extends SqlTest {
     @Test
     public void testOuterJoinEmptyFromAtsdSeries() {
         /*
-        SELECT t1.value, t2.value FROM atsd_series t1 OUTER JOIN m2 t2 WHERE t1.metric = m1
+        SELECT t1.datetime, t1.value, t2.value FROM atsd_series t1 OUTER JOIN m2 t2 WHERE t1.metric = m1
+        ORDER BY t1.datetime
 
-        | t1.value | t2.value |
-        |----------|----------|
-        | 1        | null     |
-        | 2        | null     |
-        | 3        | null     |
-        | 4        | null     |
+        | t1.datetime              | t1.value | t2.value |
+        |--------------------------|----------|----------|
+        | 2017-01-01T12:00:00.000Z | 1        | null     |
+        | 2017-01-02T12:00:00.000Z | 2        | null     |
+        | 2017-01-03T12:00:00.000Z | 3        | null     |
+        | 2017-01-04T12:00:00.000Z | 4        | null     |
          */
 
-        String sqlQuery= String.format(
-                "SELECT t1.value, t2.value " +
+        String sqlQuery = String.format(
+                "SELECT t1.datetime, t1.value, t2.value " +
                         "FROM atsd_series t1 " +
                         "OUTER JOIN '%2$s' t2 " +
-                        "WHERE t1.metric = '%1$s'",
+                        "WHERE t1.metric = '%1$s' " +
+                        "ORDER BY t1.datetime",
                 METRIC_NAME1,
                 METRIC_NAME4
         );
 
         String[][] expectedRows = {
-                {"1",    "null"},
-                {"2",    "null"},
-                {"3",    "null"},
-                {"4",    "null"},
+                {"2017-01-01T12:00:00.000Z", "1", "null"},
+                {"2017-01-02T12:00:00.000Z", "2", "null"},
+                {"2017-01-03T12:00:00.000Z", "3", "null"},
+                {"2017-01-04T12:00:00.000Z", "4", "null"},
         };
 
         assertSqlQueryRows("Wrong result for empty outer join from atsd_series", expectedRows, sqlQuery);
@@ -251,38 +262,40 @@ public class JoinWithAtsdSeriesTest extends SqlTest {
     @Test
     public void testMultipleOuterJoinFromAtsdSeries() {
         /*
-        SELECT t1.value, t2.value FROM atsd_series t1 OUTER JOIN m2 t2 WHERE t1.metric = m1
+        SELECT t1.datetime, t1.value, t2.value FROM atsd_series t1 OUTER JOIN m2 t2 WHERE t1.metric = m1
+        ORDER BY t1.datetime
 
-        | t1.value | t2.value | t3.value |
-        |----------|----------|----------|
-        | 1        | null     | null     |
-        | 2        | null     | null     |
-        | 3        | null     | null     |
-        | null     | 5        | null     |
-        | 4        | 6        | 6        |
-        | null     | 7        | 7        |
-        | null     | 8        | 8        |
+        | t1.datetime              | t1.value | t2.value | t3.value |
+        |--------------------------|----------|----------|----------|
+        | 2017-01-01T12:00:00.000Z | 1        | null     | null     |
+        | 2017-01-02T12:00:00.000Z | 2        | null     | null     |
+        | 2017-01-03T12:00:00.000Z | 3        | null     | null     |
+        | 2017-01-03T12:00:00.000Z | null     | 5        | null     |
+        | 2017-01-04T12:00:00.000Z | 4        | 6        | 6        |
+        | 2017-01-05T12:00:00.000Z | null     | 7        | 7        |
+        | 2017-01-06T12:00:00.000Z | null     | 8        | 8        |
          */
 
-        String sqlQuery= String.format(
+        String sqlQuery = String.format(
                 "SELECT t1.value, t2.value, t3.value " +
                         "FROM atsd_series t1 " +
                         "OUTER JOIN '%2$s' t2 " +
                         "OUTER JOIN '%3$s' t3 " +
-                        "WHERE t1.metric = '%1$s'",
+                        "WHERE t1.metric = '%1$s' " +
+                        "ORDER BY t1.datetime",
                 METRIC_NAME1,
                 METRIC_NAME2,
                 METRIC_NAME3
         );
 
         String[][] expectedRows = {
-                {   "1",    "null",    "null"},
-                {   "2",    "null",    "null"},
-                {   "3",    "null",    "null"},
-                {"null",       "5",    "null"},
-                {   "4",       "6",       "6"},
-                {"null",       "7",       "7"},
-                {"null",       "8",       "8"},
+                {"2017-01-01T12:00:00.000Z",    "1", "null", "null"},
+                {"2017-01-02T12:00:00.000Z",    "2", "null", "null"},
+                {"2017-01-03T12:00:00.000Z",    "3", "null", "null"},
+                {"2017-01-03T12:00:00.000Z", "null",    "5", "null"},
+                {"2017-01-04T12:00:00.000Z",    "4",    "6",    "6"},
+                {"2017-01-05T12:00:00.000Z", "null",    "7",    "7"},
+                {"2017-01-06T12:00:00.000Z", "null",    "8",    "8"},
         };
 
         assertSqlQueryRows("Wrong result for multiple outer join from atsd_series", expectedRows, sqlQuery);
@@ -294,34 +307,36 @@ public class JoinWithAtsdSeriesTest extends SqlTest {
     @Test
     public void testOuterJoinFromAtsdSeriesUsingEntity() {
         /*
-        SELECT t1.value, t2.value FROM atsd_series t1 OUTER JOIN USING ENTITY m2 t2 WHERE t1.metric = m1
+        SELECT t1.datetime, t1.value, t2.value FROM atsd_series t1 OUTER JOIN USING ENTITY m2 t2 WHERE t1.metric = m1
+        ORDER BY t1.datetime
 
-        | t1.value | t2.value |
-        |----------|----------|
-        | 1        | null     |
-        | 2        | null     |
-        | 3        | 5        |
-        | 4        | 6        |
-        | null     | 7        |
-        | null     | 8        |
+        | t1.datetime              | t1.value | t2.value |
+        |--------------------------|----------|----------|
+        | 2017-01-01T12:00:00.000Z | 1        | null     |
+        | 2017-01-02T12:00:00.000Z | 2        | null     |
+        | 2017-01-03T12:00:00.000Z | 3        | 5        |
+        | 2017-01-04T12:00:00.000Z | 4        | 6        |
+        | 2017-01-05T12:00:00.000Z | null     | 7        |
+        | 2017-01-06T12:00:00.000Z | null     | 8        |
          */
 
-        String sqlQuery= String.format(
-                "SELECT t1.value, t2.value " +
+        String sqlQuery = String.format(
+                "SELECT t1.datetime, t1.value, t2.value " +
                         "FROM atsd_series t1 " +
                         "OUTER JOIN USING ENTITY '%2$s' t2 " +
-                        "WHERE t1.metric = '%1$s'",
+                        "WHERE t1.metric = '%1$s' " +
+                        "ORDER BY t1.datetime",
                 METRIC_NAME1,
                 METRIC_NAME2
         );
 
         String[][] expectedRows = {
-                {   "1",    "null"},
-                {   "2",    "null"},
-                {   "3",       "5"},
-                {   "4",       "6"},
-                {"null",       "7"},
-                {"null",       "8"},
+                {"2017-01-01T12:00:00.000Z",    "1", "null"},
+                {"2017-01-02T12:00:00.000Z",    "2", "null"},
+                {"2017-01-03T12:00:00.000Z",    "3",    "5"},
+                {"2017-01-04T12:00:00.000Z",    "4",    "6"},
+                {"2017-01-05T12:00:00.000Z", "null",    "7"},
+                {"2017-01-06T12:00:00.000Z", "null",    "8"},
         };
 
         assertSqlQueryRows("Wrong result for outer join using entity from atsd_series", expectedRows, sqlQuery);
@@ -333,16 +348,18 @@ public class JoinWithAtsdSeriesTest extends SqlTest {
     @Test
     public void testSelfJoinFromAtsdSeries() {
         /*
-        SELECT t1.value, t2.value FROM atsd_series t1 OUTER JOIN m1 t2 WHERE t1.metric = m1
+        SELECT t1.datetime, t1.value, t2.value FROM atsd_series t1 OUTER JOIN m1 t2 WHERE t1.metric = m1
+        ORDER BY t1.datetime
 
         Self-join, error expected
          */
 
-        String sqlQuery= String.format(
-                "SELECT t1.value, t2.value " +
+        String sqlQuery = String.format(
+                "SELECT t1.datetime, t1.value, t2.value " +
                         "FROM atsd_series t1 " +
                         "JOIN '%1$s' t2 " +
-                        "WHERE t1.metric = '%1$s'",
+                        "WHERE t1.metric = '%1$s' " +
+                        "ORDER BY t1.datetime",
                 METRIC_NAME1
         );
 
