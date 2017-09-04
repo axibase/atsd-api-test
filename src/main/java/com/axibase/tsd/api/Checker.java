d25606fc1b5a22bfd760a4dc57508e532315060a
Anton Rybochkin <anton.rybochkin@axibase.com>
2017-09-04 11:49:04 +0300

    try to fix checker

--- src/main/java/com/axibase/tsd/api/Checker.java
+++ src/main/java/com/axibase/tsd/api/Checker.java
@@ -4,22 +4,21 @@ package com.axibase.tsd.api;
 import com.axibase.tsd.api.method.BaseMethod;
 import com.axibase.tsd.api.method.checks.AbstractCheck;
 import com.axibase.tsd.api.util.NotCheckedException;
+import lombok.SneakyThrows;
 
 public class Checker {
+    @SneakyThrows(InterruptedException.class)
     public static void check(AbstractCheck check) {
-        Long startTime = System.currentTimeMillis();
-        Boolean result = false;
-        do {
-            result = check.isChecked();
-            if (System.currentTimeMillis() - BaseMethod.UPPER_BOUND_FOR_CHECK > startTime) {
-                throw new NotCheckedException(check.getErrorMessage());
-            } else {
-                try {
-                    Thread.sleep(BaseMethod.REQUEST_INTERVAL);
-                } catch (InterruptedException e) {
-                    throw new IllegalStateException(e.getMessage());
+        final long startTime = System.currentTimeMillis();
+        boolean checked = false;
+        while (!checked) {
+            checked = check.isChecked();
+            if (!checked) {
+                if (System.currentTimeMillis() - BaseMethod.UPPER_BOUND_FOR_CHECK > startTime) {
+                    throw new NotCheckedException(check.getErrorMessage());
                 }
+                Thread.sleep(BaseMethod.REQUEST_INTERVAL);
             }
-        } while (!result);
+        }
     }
 }
