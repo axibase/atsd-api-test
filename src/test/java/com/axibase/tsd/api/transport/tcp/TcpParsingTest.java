2ff70d7add32958f6c6c20b7015100e8d8c3a7b9
Valery Tolstov <me@vtolstov.org>
2017-10-12 16:06:03 +0300

    Tests for #4411

---
+++ src/test/java/com/axibase/tsd/api/transport/tcp/TcpParsingTest.java
@@ -0,0 +1,133 @@
+package com.axibase.tsd.api.transport.tcp;
+
+import com.axibase.tsd.api.Checker;
+import com.axibase.tsd.api.method.checks.SeriesCheck;
+import com.axibase.tsd.api.method.metric.MetricMethod;
+import com.axibase.tsd.api.method.series.SeriesMethod;
+import com.axibase.tsd.api.model.metric.Metric;
+import com.axibase.tsd.api.model.series.Series;
+import com.axibase.tsd.api.model.series.SeriesQuery;
+import com.axibase.tsd.api.util.Mocks;
+import com.axibase.tsd.api.util.Util;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.qameta.allure.Issue;
+import org.apache.commons.io.FileUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TcpParsingTest {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final String COMMANDS_FILE = "commands.txt";
+    private static final String METRICS_DIR = "metrics";
+    private static final String SERIES_DIR = "series";
+
+    private static File getFile(String name) {
+        URL resourceUrl = TcpParsingTest.class.getResource(name);
+        if (resourceUrl != null) {
+            try {
+                return new File(resourceUrl.toURI());
+            } catch (URISyntaxException e) {
+                /* Shouldn't happen */
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }
+        return null;
+    }
+
+    private static File getFile(Path path) {
+        return getFile(path.toString());
+    }
+
+    private String readFile(Path filePath) throws IOException {
+        return FileUtils.readFileToString(getFile(filePath));
+    }
+
+    @DataProvider
+    public Object[][] provideTestDirectories() {
+        Path root = Paths.get("test_set");
+        File rootFile = getFile(root);
+        File[] list = rootFile.listFiles();
+        List<Object[]> resultList = new ArrayList<>();
+        if (list != null) {
+            for (File testDir : list) {
+                resultList.add(new Object[]{root.resolve(testDir.getName())});
+            }
+        }
+        Object[][] result = new Object[resultList.size()][];
+        resultList.toArray(result);
+        return result;
+    }
+
+    private void checkMetrics(Path testDir) throws IOException, JSONException {
+        Path metricsPath = testDir.resolve(METRICS_DIR);
+        File metricsDir = getFile(metricsPath);
+        if (metricsDir == null) {
+            return;
+        }
+        File[] metricFiles = metricsDir.listFiles();
+        if (metricFiles != null) {
+            for (File metricFile : metricFiles) {
+                String expectedJson = FileUtils.readFileToString(metricFile);
+                Metric expectedMetric = MAPPER.readValue(expectedJson, Metric.class);
+                String metricName = expectedMetric.getName();
+                String actualJson = MetricMethod.queryMetric(metricName).readEntity(String.class);
+                JSONAssert.assertEquals(expectedJson, actualJson, JSONCompareMode.LENIENT);
+            }
+        }
+    }
+
+    private void checkSeries(Path testDir) throws IOException, JSONException {
+        Path seriesPath = testDir.resolve(SERIES_DIR);
+        File seriesDir = getFile(seriesPath);
+        if (seriesDir == null) {
+            return;
+        }
+        File[] seriesFiles = seriesDir.listFiles();
+        if (seriesFiles != null) {
+            for (File seriesFile : seriesFiles) {
+                String expectedJson = FileUtils.readFileToString(seriesFile);
+                JSONObject expectedObject = new JSONArray(expectedJson).getJSONObject(0);
+                SeriesQuery query = new SeriesQuery(
+                        expectedObject.getString("entity"),
+                        expectedObject.getString("metric"),
+                        Util.MIN_QUERYABLE_DATE,
+                        Util.MAX_QUERYABLE_DATE
+                );
+                String actualJson = SeriesMethod.querySeries(query).readEntity(String.class);
+                JSONAssert.assertEquals(expectedJson, actualJson, JSONCompareMode.LENIENT);
+            }
+        }
+    }
+
+    @Issue("4411")
+    @Test(dataProvider = "provideTestDirectories")
+    public void testNetworkCommandParser(Path testDir) throws Exception {
+        /* Control series to wait for TCP commands are complete */
+        Series controlSeries = Mocks.series();
+
+        String fileContents = readFile(testDir.resolve(COMMANDS_FILE));
+        TCPSender.send(fileContents + '\n' + controlSeries.toCommands().get(0).toString());
+
+        Thread.sleep(2000);
+        Checker.check(new SeriesCheck(Collections.singletonList(controlSeries)));
+
+        checkMetrics(testDir);
+        checkSeries(testDir);
+    }
+}
