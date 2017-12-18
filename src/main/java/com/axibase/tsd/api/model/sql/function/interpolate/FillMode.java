06c949e84c8390254b18d86bc69d8251e50aca7a
Valery Tolstov <me@vtolstov.org>
2017-12-18 14:44:17 +0300

    Tests and updates for #4803

--- src/main/java/com/axibase/tsd/api/model/sql/function/interpolate/FillMode.java
+++ src/main/java/com/axibase/tsd/api/model/sql/function/interpolate/FillMode.java
@@ -1,5 +1,53 @@
 package com.axibase.tsd.api.model.sql.function.interpolate;
 
-public enum FillMode {
-    NONE, NAN, EXTEND
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class FillMode {
+    private static final Set<String> STRING_VALUE_SET = new HashSet<>();
+
+    public static final FillMode NONE = new FillMode("NONE");
+    public static final FillMode NAN = new FillMode("NAN");
+    public static final FillMode EXTEND = new FillMode("EXTEND");
+
+    public static final FillMode TRUE = new FillMode("TRUE");
+    public static final FillMode YES = new FillMode("YES");
+    public static final FillMode FALSE = new FillMode("FALSE");
+    public static final FillMode NO = new FillMode("NO");
+
+    private String mode;
+
+    private FillMode(String mode, boolean extra) {
+        this.mode = mode;
+        if (!extra) {
+            STRING_VALUE_SET.add(mode);
+        }
+    }
+
+    private FillMode(String mode) {
+        this(mode, false);
+    }
+
+    public static FillMode value(Double val) {
+        String mode;
+        if (val == null) {
+            mode = "NAN";
+        } else {
+            mode = "VALUE " + String.valueOf(val);
+        }
+        return new FillMode(mode, true);
+    }
+
+    public static List<String> stringValues() {
+        List<String> result = new ArrayList<>();
+        result.addAll(STRING_VALUE_SET);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return mode;
+    }
 }
