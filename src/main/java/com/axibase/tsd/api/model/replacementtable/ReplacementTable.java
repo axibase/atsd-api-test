62dbeaa346fd8fca77089b75113da524717042a3
Kamakin Kirill <k.kamakin@protonmail.ch>
2018-06-15 18:12:16 +0300

    Unnecessary constructors

--- src/main/java/com/axibase/tsd/api/model/replacementtable/ReplacementTable.java
+++ src/main/java/com/axibase/tsd/api/model/replacementtable/ReplacementTable.java
@@ -1,6 +1,5 @@
 package com.axibase.tsd.api.model.replacementtable;
 
-import com.axibase.tsd.api.util.Registry;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,31 +15,8 @@ public class ReplacementTable {
     private SupportedFormat format;
     private Map<String, String> keys = new HashMap<>();
 
-    public ReplacementTable(String name, Map<String, String> keys) {
-        Registry.ReplacementTable.checkExists(name);
-        this.name = name;
-        this.keys = keys;
-    }
-
     public ReplacementTable(String name) {
-        Registry.ReplacementTable.checkExists(name);
         this.name = name;
-    }
-
-    public ReplacementTable(String name, String author, String description) {
-        Registry.ReplacementTable.checkExists(name);
-        this.name = name;
-        this.author = author;
-        this.description = description;
-    }
-
-    public ReplacementTable(String name, String description, String author, SupportedFormat format, Map<String, String> keys) {
-        Registry.ReplacementTable.checkExists(name);
-        this.name = name;
-        this.description = description;
-        this.author = author;
-        this.format = format;
-        this.keys = keys;
     }
 
     public void addValue(String key, String value){
