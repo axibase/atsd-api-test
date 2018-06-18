fcf3630d69e179c92d8a8fad5f2e2df545038ce5
Kamakin Kirill <k.kamakin@protonmail.ch>
2018-06-18 13:47:37 +0300

    Using accessors, reflection fix

--- src/main/java/com/axibase/tsd/api/model/replacementtable/ReplacementTable.java
+++ src/main/java/com/axibase/tsd/api/model/replacementtable/ReplacementTable.java
@@ -2,13 +2,17 @@ package com.axibase.tsd.api.model.replacementtable;
 
 import com.axibase.tsd.api.util.Registry;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@Accessors(chain = true)
 public class ReplacementTable {
     private String name;
     private String description;
@@ -17,8 +21,7 @@ public class ReplacementTable {
     private Map<String, String> keys = new HashMap<>();
 
     private ReplacementTable(String name, SupportedFormat format) {
-        this.name = name;
-        this.format = format;
+        this.setName(name).setFormat(format);
     }
 
     public static ReplacementTable of(String name, SupportedFormat format) {
