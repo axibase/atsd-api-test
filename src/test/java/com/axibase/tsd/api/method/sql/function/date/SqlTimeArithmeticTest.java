2d219573e82ce9c967f1b49d0a8547c3e2708542
Kamakin Kirill <k.kamakin@protonmail.ch>
2018-07-26 16:31:06 +0300

    Refactor, add tests with SELECT, WHERE, HAVING, GROUP BY clauses

--- src/test/java/com/axibase/tsd/api/method/sql/function/date/SqlTimeArithmeticTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/function/date/SqlTimeArithmeticTest.java
@@ -10,6 +10,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.time.ZonedDateTime;
 import java.util.Arrays;
 
 import static com.axibase.tsd.api.util.Mocks.entity;
@@ -19,16 +20,19 @@ import static org.apache.commons.lang3.ArrayUtils.toArray;
 public class SqlTimeArithmeticTest extends SqlTest {
     private static final String METRIC_NAME = metric();
     private static final String ENTITY_NAME = entity();
+    private static final ZonedDateTime twentyFirst = ZonedDateTime.parse("2018-07-21T00:00:00Z");
+    private static final ZonedDateTime twentySecond = ZonedDateTime.parse("2018-07-22T00:00:00Z");
+    private static final ZonedDateTime twentyThird = ZonedDateTime.parse("2018-07-23T00:00:00Z");
 
     @BeforeClass
     public static void prepareData() throws Exception {
         final Series series = new Series(ENTITY_NAME, METRIC_NAME)
                 // Saturday (is not weekday, next day is not weekday)
-                .addSamples(Sample.ofDateInteger("2018-07-21T12:54:00+0300", 20))
+                .addSamples(Sample.ofTimeInteger(twentyFirst.toEpochSecond() * 1000, 20))
                 // Sunday (is not weekday, next day is weekday)
-                .addSamples(Sample.ofDateInteger("2018-07-22T12:54:00+0300", 20))
+                .addSamples(Sample.ofTimeInteger(twentySecond.toEpochSecond() * 1000, 20))
                 // Monday (is weekday, next day is weekday)
-                .addSamples(Sample.ofDateInteger("2018-07-23T12:54:00+0300", 20));
+                .addSamples(Sample.ofTimeInteger(twentyThird.toEpochSecond() * 1000, 20));
 
         SeriesMethod.insertSeriesCheck(series);
     }
@@ -76,5 +80,104 @@ public class SqlTimeArithmeticTest extends SqlTest {
                 .toArray(String[][]::new);
         final String assertMessage = String.format("Fail to calculate \"%s\"", expression);
         assertSqlQueryRows(assertMessage, expectedRows, query);
+    }
+
+    @DataProvider
+    public Object[][] provideClauses() {
+        return toArray(
+                testCase(String.format("SELECT time + 1000*60*60*24*1 FROM \"%s\"", METRIC_NAME),
+                        String.valueOf(twentyFirst.plusSeconds(60 * 60 * 24).toEpochSecond() * 1000),
+                        String.valueOf(twentySecond.plusSeconds(60 * 60 * 24).toEpochSecond() * 1000),
+                        String.valueOf(twentyThird.plusSeconds(60 * 60 * 24).toEpochSecond() * 1000)
+                ),
+                testCase(String.format("SELECT time - 1000*60*60*24*1 FROM \"%s\"", METRIC_NAME),
+                        String.valueOf(twentyFirst.minusSeconds(60 * 60 * 24).toEpochSecond() * 1000),
+                        String.valueOf(twentySecond.minusSeconds(60 * 60 * 24).toEpochSecond() * 1000),
+                        String.valueOf(twentyThird.minusSeconds(60 * 60 * 24).toEpochSecond() * 1000)
+                ),
+                testCase(String.format("SELECT time - 1000*60*60*24*2 FROM \"%s\"", METRIC_NAME),
+                        String.valueOf(twentyFirst.minusSeconds(60 * 60 * 24 * 2).toEpochSecond() * 1000),
+                        String.valueOf(twentySecond.minusSeconds(60 * 60 * 24 * 2).toEpochSecond() * 1000),
+                        String.valueOf(twentyThird.minusSeconds(60 * 60 * 24 * 2).toEpochSecond() * 1000)
+                ),
+                testCase(String.format("SELECT time + 1000*60*60*24*2 FROM \"%s\"", METRIC_NAME),
+                        String.valueOf(twentyFirst.plusSeconds(60 * 60 * 24 * 2).toEpochSecond() * 1000),
+                        String.valueOf(twentySecond.plusSeconds(60 * 60 * 24 * 2).toEpochSecond() * 1000),
+                        String.valueOf(twentyThird.plusSeconds(60 * 60 * 24 * 2).toEpochSecond() * 1000)
+                ),
+                testCase(String.format("SELECT time + 1000*60*60*24*1 FROM \"%s\" GROUP BY time + 1000*60*60*24*1", METRIC_NAME),
+                        String.valueOf(twentyFirst.plusSeconds(60 * 60 * 24).toEpochSecond() * 1000),
+                        String.valueOf(twentySecond.plusSeconds(60 * 60 * 24).toEpochSecond() * 1000),
+                        String.valueOf(twentyThird.plusSeconds(60 * 60 * 24).toEpochSecond() * 1000)
+                ),
+                testCase(String.format("SELECT time - 1000*60*60*24*1 FROM \"%s\" GROUP BY time - 1000*60*60*24*1", METRIC_NAME),
+                        String.valueOf(twentyFirst.minusSeconds(60 * 60 * 24).toEpochSecond() * 1000),
+                        String.valueOf(twentySecond.minusSeconds(60 * 60 * 24).toEpochSecond() * 1000),
+                        String.valueOf(twentyThird.minusSeconds(60 * 60 * 24).toEpochSecond() * 1000)
+                ),
+                testCase(String.format("SELECT time - 1000*60*60*24*2 FROM \"%s\" GROUP BY time - 1000*60*60*24*2", METRIC_NAME),
+                        String.valueOf(twentyFirst.minusSeconds(60 * 60 * 24 * 2).toEpochSecond() * 1000),
+                        String.valueOf(twentySecond.minusSeconds(60 * 60 * 24 * 2).toEpochSecond() * 1000),
+                        String.valueOf(twentyThird.minusSeconds(60 * 60 * 24 * 2).toEpochSecond() * 1000)
+                ),
+                testCase(String.format("SELECT time + 1000*60*60*24*2 FROM \"%s\" GROUP BY time + 1000*60*60*24*2", METRIC_NAME),
+                        String.valueOf(twentyFirst.plusSeconds(60 * 60 * 24 * 2).toEpochSecond() * 1000),
+                        String.valueOf(twentySecond.plusSeconds(60 * 60 * 24 * 2).toEpochSecond() * 1000),
+                        String.valueOf(twentyThird.plusSeconds(60 * 60 * 24 * 2).toEpochSecond() * 1000)
+                ),
+                testCase(String.format("SELECT time FROM \"%s\" WHERE time + 1000*60*60*24*1 BETWEEN '2018-01-01' AND '2019-01-01'", METRIC_NAME),
+                        String.valueOf(twentyFirst.toEpochSecond() * 1000),
+                        String.valueOf(twentySecond.toEpochSecond() * 1000),
+                        String.valueOf(twentyThird.toEpochSecond() * 1000)
+                ),
+                testCase(String.format("SELECT time FROM \"%s\" WHERE time - 1000*60*60*24*1 BETWEEN '2018-01-01' AND '2019-01-01'", METRIC_NAME),
+                        String.valueOf(twentyFirst.toEpochSecond() * 1000),
+                        String.valueOf(twentySecond.toEpochSecond() * 1000),
+                        String.valueOf(twentyThird.toEpochSecond() * 1000)
+                ),
+                testCase(String.format("SELECT time FROM \"%s\" WHERE time - 1000*60*60*24*2 BETWEEN '2018-01-01' AND '2019-01-01'", METRIC_NAME),
+                        String.valueOf(twentyFirst.toEpochSecond() * 1000),
+                        String.valueOf(twentySecond.toEpochSecond() * 1000),
+                        String.valueOf(twentyThird.toEpochSecond() * 1000)
+                ),
+                testCase(String.format("SELECT time FROM \"%s\" WHERE time + 1000*60*60*24*2 BETWEEN '2018-01-01' AND '2019-01-01'", METRIC_NAME),
+                        String.valueOf(twentyFirst.toEpochSecond() * 1000),
+                        String.valueOf(twentySecond.toEpochSecond() * 1000),
+                        String.valueOf(twentyThird.toEpochSecond() * 1000)
+                ),
+                testCase(String.format("SELECT time FROM \"%s\" GROUP BY PERIOD(1 day, 'UTC') HAVING time + 1000*60*60*24*1 BETWEEN '2018-01-01' AND '2019-01-01'", METRIC_NAME),
+                        String.valueOf(twentyFirst.toEpochSecond() * 1000),
+                        String.valueOf(twentySecond.toEpochSecond() * 1000),
+                        String.valueOf(twentyThird.toEpochSecond() * 1000)
+                ),
+                testCase(String.format("SELECT time FROM \"%s\" GROUP BY PERIOD(1 day, 'UTC') HAVING time - 1000*60*60*24*1 BETWEEN '2018-01-01' AND '2019-01-01'", METRIC_NAME),
+                        String.valueOf(twentyFirst.toEpochSecond() * 1000),
+                        String.valueOf(twentySecond.toEpochSecond() * 1000),
+                        String.valueOf(twentyThird.toEpochSecond() * 1000)
+                ),
+                testCase(String.format("SELECT time FROM \"%s\" GROUP BY PERIOD(1 day, 'UTC') HAVING time - 1000*60*60*24*2 BETWEEN '2018-01-01' AND '2019-01-01'", METRIC_NAME),
+                        String.valueOf(twentyFirst.toEpochSecond() * 1000),
+                        String.valueOf(twentySecond.toEpochSecond() * 1000),
+                        String.valueOf(twentyThird.toEpochSecond() * 1000)
+                ),
+                testCase(String.format("SELECT time FROM \"%s\" GROUP BY PERIOD(1 day, 'UTC') HAVING time + 1000*60*60*24*2 BETWEEN '2018-01-01' AND '2019-01-01'", METRIC_NAME),
+                        String.valueOf(twentyFirst.toEpochSecond() * 1000),
+                        String.valueOf(twentySecond.toEpochSecond() * 1000),
+                        String.valueOf(twentyThird.toEpochSecond() * 1000)
+                )
+        );
+    }
+
+    @Issue("5492")
+    @Test(
+            description = "Test support of mathematical operators in clauses",
+            dataProvider = "provideClauses"
+    )
+    public void testSqlClauseTimeMathOperations(final String clause, final String[] results) {
+        final String[][] expectedRows = Arrays.stream(results)
+                .map(ArrayUtils::toArray)
+                .toArray(String[][]::new);
+        final String assertMessage = String.format("Fail to calculate \"%s\"", clause);
+        assertSqlQueryRows(assertMessage, expectedRows, clause);
     }
 }
