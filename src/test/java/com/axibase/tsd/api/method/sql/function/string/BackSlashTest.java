8f0a067cb213394034d48819335b34ac6b50353f
Kamakin Kirill <k.kamakin@protonmail.ch>
2018-08-22 15:54:14 +0300

    Add UPPER, LOWER, LENGTH tests

--- src/test/java/com/axibase/tsd/api/method/sql/function/string/BackSlashTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/function/string/BackSlashTest.java
@@ -4,6 +4,7 @@ import com.axibase.tsd.api.method.sql.SqlTest;
 import com.axibase.tsd.api.model.series.Sample;
 import com.axibase.tsd.api.model.series.Series;
 import io.qameta.allure.Issue;
+import org.apache.commons.lang3.ArrayUtils;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -41,9 +42,10 @@ public class BackSlashTest extends SqlTest {
     }
 
     @DataProvider
-    public static Object[][] provideReplaceResults() {
-        final String replaceFormat = "REPLACE(text, '%s', 'Y')";
-        final String[] queries = Stream.of(CHARACTERS).map((character) -> String.format(replaceFormat, character))
+    public static Object[][] provideReplace() {
+        final Object[][] result = new Object[CHARACTERS.length][CHARACTERS.length];
+        final String[] queries = Stream.of(CHARACTERS)
+                .map((character) -> String.format("REPLACE(text, '%s', 'Y')", character))
                 .toArray(String[]::new);
         final Map<String, String[][]> results = new HashMap<>();
         for (final String character : CHARACTERS) {
@@ -53,21 +55,48 @@ public class BackSlashTest extends SqlTest {
             }
             results.put(character, strings);
         }
-        final Object[][] result = new Object[CHARACTERS.length][CHARACTERS.length];
         for (int i = 0; i < CHARACTERS.length; i++) {
-            result[i] = new Object[]{
-                    queries[i], results.get(CHARACTERS[i])
-            };
+            result[i] = toArray(queries[i], results.get(CHARACTERS[i]));
         }
         return result;
     }
 
+    @DataProvider
+    public static Object[][] provideSingleArg() {
+        return toArray(
+                toArray("UPPER", Stream.of(CHARACTERS).map((s) -> String.format(FORMAT, s).toUpperCase())
+                        .map(ArrayUtils::toArray)
+                        .toArray(String[][]::new)
+                ),
+                toArray("LOWER", Stream.of(CHARACTERS).map((s) -> String.format(FORMAT, s).toLowerCase())
+                        .map(ArrayUtils::toArray)
+                        .toArray(String[][]::new)
+                ),
+                toArray("LENGTH", Stream.of(CHARACTERS).map((s) -> String.format(FORMAT, s).length())
+                        .map(String::valueOf)
+                        .map(ArrayUtils::toArray)
+                        .toArray(String[][]::new)
+                )
+        );
+    }
+
     @Issue("5600")
     @Test(
-            dataProvider = "provideReplaceResults"
+            dataProvider = "provideReplace",
+            description = "Test REPLACE() function with escaped characters"
     )
     public void testReplace(final String query, final String[][] results) {
         final String sqlQuery = String.format("SELECT %s FROM \"%s\"", query, METRIC_NAME);
         assertSqlQueryRows("Fail to replace an escaped character", results, sqlQuery);
+    }
+
+    @Issue("5600")
+    @Test(
+            dataProvider = "provideSingleArg",
+            description = "Test single argument with escaped characters"
+    )
+    public void testSingleArg(final String func, final String[][] results) {
+        final String query = String.format("SELECT %s(text) FROM \"%s\"", func, METRIC_NAME);
+        assertSqlQueryRows("Fail to use an espaced character in a single argument function", results, query);
     }
 }
