d6e212a7193df820491e0906a779d9412ae0fa87
sergei <gray4747@gmail.com>
2018-11-16 20:58:32 +0300

    Added infinity tests

--- src/test/java/com/axibase/tsd/api/method/sql/function/math/SqlTrigonometricFunctionTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/function/math/SqlTrigonometricFunctionTest.java
@@ -1,16 +1,11 @@
 package com.axibase.tsd.api.method.sql.function.math;
 
-import com.axibase.tsd.api.method.sql.SqlMethod;
 import com.axibase.tsd.api.method.sql.SqlTest;
 import com.axibase.tsd.api.model.sql.StringTable;
 import io.qameta.allure.Issue;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import javax.ws.rs.core.Response;
-
-import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 
 public class SqlTrigonometricFunctionTest extends SqlTest {
 
@@ -23,11 +18,28 @@ public class SqlTrigonometricFunctionTest extends SqlTest {
     }
 
     @DataProvider
+    public static Object[][] provideAsinParameters() {
+        return new Object[][]{{"-1", "-1.5707963267948966"}, {"-1/2", "-0.5235987755982989"}, {"0", "0.0"},
+                {"1/2", "0.5235987755982989"}, {"1", "1.5707963267948966"}};
+    }
+
+    @DataProvider
+    public static Object[][] provideProhibitedAsinAcosParameters() {
+        return new Object[][]{{"-2"}, {"2"}};
+    }
+
+    @DataProvider
     public static Object[][] provideCosParameters() {
         return new Object[][]{{"-pi()", "-1.0"}, {"-pi()*3/4", "-0.7071067811865475"},
                 {"-pi()/2", "6.123233995736766E-17"}, {"-pi()/4", "0.7071067811865476"}, {"0", "1.0"},
                 {"pi()/4", "0.7071067811865476"}, {"pi()/2", "6.123233995736766E-17"},
                 {"pi()*3/4", "-0.7071067811865475"}, {"pi()", "-1.0"}};
+    }
+
+    @DataProvider
+    public static Object[][] provideAcosParameters() {
+        return new Object[][]{{"-1", "3.141592653589793"}, {"-1/2", "2.0943951023931957"}, {"0", "1.5707963267948966"},
+                {"1/2", "1.0471975511965979"}, {"1", "0.0"}};
     }
 
     @DataProvider
@@ -39,11 +51,6 @@ public class SqlTrigonometricFunctionTest extends SqlTest {
     }
 
     @DataProvider
-    public static Object[][] provideProhibitedCotParameters() {
-        return new Object[][]{{"pi()"}, {"0"}};
-    }
-
-    @DataProvider
     public static Object[][] provideTanParameters() {
         return new Object[][]{{"-pi()", "1.2246467991473532E-16"}, {"-pi()*3/4", "1.0000000000000002"},
                 {"-pi()/4", "-0.9999999999999999"}, {"0", "0.0"}, {"pi()/4", "0.9999999999999999"},
@@ -51,8 +58,10 @@ public class SqlTrigonometricFunctionTest extends SqlTest {
     }
 
     @DataProvider
-    public static Object[][] provideProhibitedTanParameters() {
-        return new Object[][]{{"pi()/2"}};
+    public static Object[][] provideAtanParameters() {
+        return new Object[][]{{"-1/0", "-1.5707963267948966"}, {"-1", "-0.7853981633974483"},
+                {"-1/2", "-0.4636476090008061"}, {"0", "0.0"}, {"1/2", "0.4636476090008061"},
+                {"1", "0.7853981633974483"}, {"1/0", "1.5707963267948966"}};
     }
 
     @DataProvider
@@ -74,9 +83,35 @@ public class SqlTrigonometricFunctionTest extends SqlTest {
     }
 
     @Issue("5764")
+    @Test(dataProvider = "provideAsinParameters")
+    public void testAsin(String functionParameter, String expectedResult) {
+        String sqlQuery = String.format("SELECT ASIN(%s)", functionParameter);
+
+        StringTable resultTable = queryResponse(sqlQuery).readEntity(StringTable.class);
+
+        Assert.assertEquals(resultTable.getTableMetaData().getColumnMeta(0).getDataType(), "double",
+                "Sin(" + functionParameter + ") wrong data type");
+        Assert.assertEquals(resultTable.getRows().get(0).get(0), expectedResult,
+                "Sin(" + functionParameter + ") wrong result");
+    }
+
+    @Issue("5764")
     @Test(dataProvider = "provideCosParameters")
     public void testCos(String functionParameter, String expectedResult) {
         String sqlQuery = String.format("SELECT COS(%s)", functionParameter);
+
+        StringTable resultTable = queryResponse(sqlQuery).readEntity(StringTable.class);
+
+        Assert.assertEquals(resultTable.getTableMetaData().getColumnMeta(0).getDataType(), "double",
+                "Cos(" + functionParameter + ") wrong data type");
+        Assert.assertEquals(resultTable.getRows().get(0).get(0), expectedResult,
+                "Cos(" + functionParameter + ") wrong result");
+    }
+
+    @Issue("5764")
+    @Test(dataProvider = "provideAcosParameters")
+    public void testAcos(String functionParameter, String expectedResult) {
+        String sqlQuery = String.format("SELECT ACOS(%s)", functionParameter);
 
         StringTable resultTable = queryResponse(sqlQuery).readEntity(StringTable.class);
 
@@ -113,19 +148,52 @@ public class SqlTrigonometricFunctionTest extends SqlTest {
     }
 
     @Issue("5764")
+    @Test(dataProvider = "provideAtanParameters")
+    public void testAtan(String functionParameter, String expectedResult) {
+        String sqlQuery = String.format("SELECT ATAN(%s)", functionParameter);
+
+        StringTable resultTable = queryResponse(sqlQuery).readEntity(StringTable.class);
+
+        Assert.assertEquals(resultTable.getTableMetaData().getColumnMeta(0).getDataType(), "double",
+                "Tan(" + functionParameter + ") wrong data type");
+        Assert.assertEquals(resultTable.getRows().get(0).get(0), expectedResult,
+                "Tan(" + functionParameter + ") wrong result");
+    }
+
+    @Issue("5764")
     @Test(dataProvider = "provideFunctionNames")
     public void testNullValue(String functionParameter) {
-        String sqlQuery = String.format("SELECT %s(null)", functionParameter);
+        String sqlQuery = String.format("SELECT %s(cast(null as number))", functionParameter);
 
-        Response response = SqlMethod.queryResponse(sqlQuery);
+        StringTable resultTable = queryResponse(sqlQuery).readEntity(StringTable.class);
 
-        Assert.assertEquals(response.getStatus(), BAD_REQUEST.getStatusCode());
+        Assert.assertEquals(resultTable.getRows().get(0).get(0), "NaN", "Wrong response");
     }
 
     @Issue("5764")
     @Test(dataProvider = "provideFunctionNames")
     public void testNanValue(String functionParameter) {
         String sqlQuery = String.format("SELECT %s(NaN)", functionParameter);
+
+        StringTable resultTable = queryResponse(sqlQuery).readEntity(StringTable.class);
+
+        Assert.assertEquals(resultTable.getRows().get(0).get(0), "NaN", "Wrong response");
+    }
+
+    @Issue("5764")
+    @Test(dataProvider = "provideProhibitedAsinAcosParameters")
+    public void testAsinProhibitedValue(String functionParameter) {
+        String sqlQuery = String.format("SELECT ASIN(%s)", functionParameter);
+
+        StringTable resultTable = queryResponse(sqlQuery).readEntity(StringTable.class);
+
+        Assert.assertEquals(resultTable.getRows().get(0).get(0), "NaN", "Wrong response");
+    }
+
+    @Issue("5764")
+    @Test(dataProvider = "provideProhibitedAsinAcosParameters")
+    public void testAcosProhibitedValue(String functionParameter) {
+        String sqlQuery = String.format("SELECT ACOS(%s)", functionParameter);
 
         StringTable resultTable = queryResponse(sqlQuery).readEntity(StringTable.class);
 
