d7252d4a81e782cf7f5bd46f1318be26ad7ff792
sergei <gray4747@gmail.com>
2018-11-19 15:46:15 +0300

    Created enum for trigonometric functions

--- src/test/java/com/axibase/tsd/api/method/sql/function/math/SqlTrigonometricFunctionTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/function/math/SqlTrigonometricFunctionTest.java
@@ -7,14 +7,54 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.function.Function;
+
 public class SqlTrigonometricFunctionTest extends SqlTest {
+    private static double DELTA = 1.0E-15;
+
+    private enum TrigonometricAccessor {
+        SIN("SIN", Math::sin),
+        COS("COS", Math::cos),
+        TAN("TAN", Math::tan),
+        COT("COT", param -> 1 / Math.tan(param));
+
+        String functionName;
+        Function<Double, Double> trigonometricFunction;
+
+        TrigonometricAccessor(String functionName, Function<Double, Double> trigonometricFunction) {
+            this.functionName = functionName;
+            this.trigonometricFunction = trigonometricFunction;
+        }
+
+        double apply(double parameter) {
+            return trigonometricFunction.apply(parameter);
+        }
+    }
+
+    private static Object[][] getArguments() {
+        return new Object[][]{{"-pi()", -Math.PI},
+                {"-pi()*3/4", -Math.PI * 3 / 4},
+                {"-pi()/2", -Math.PI / 2},
+                {"-pi()/4", -Math.PI / 4},
+                {"0", 0.0},
+                {"pi()/4", Math.PI / 4},
+                {"pi()/2", Math.PI / 2},
+                {"pi()*3/4", Math.PI * 3 / 4},
+                {"pi()", Math.PI}};
+    }
 
     @DataProvider
-    public static Object[][] provideSinParameters() {
-        return new Object[][]{{"-pi()", "-1.2246467991473532E-16"}, {"-pi()*3/4", "-0.7071067811865476"},
-                {"-pi()/2", "-1.0"}, {"-pi()/4", "-0.7071067811865475"}, {"0", "0.0"},
-                {"pi()/4", "0.7071067811865475"}, {"pi()/2", "1.0"}, {"pi()*3/4", "0.7071067811865476"},
-                {"pi()", "1.2246467991473532E-16"}};
+    public static Object[][] provideTrigonometricValues() {
+        TrigonometricAccessor[] accessors = TrigonometricAccessor.values();
+        Object[][] arguments = getArguments();
+        Object[][] result = new Object[arguments.length * accessors.length][];
+
+        for (int i = 0; i < accessors.length; i++)
+            for (int j = 0; j < arguments.length; j++) {
+                result[i * arguments.length + j] = new Object[]{arguments[j][0], arguments[j][1], accessors[i]};
+            }
+
+        return result;
     }
 
     @DataProvider
@@ -29,32 +69,9 @@ public class SqlTrigonometricFunctionTest extends SqlTest {
     }
 
     @DataProvider
-    public static Object[][] provideCosParameters() {
-        return new Object[][]{{"-pi()", "-1.0"}, {"-pi()*3/4", "-0.7071067811865475"},
-                {"-pi()/2", "6.123233995736766E-17"}, {"-pi()/4", "0.7071067811865476"}, {"0", "1.0"},
-                {"pi()/4", "0.7071067811865476"}, {"pi()/2", "6.123233995736766E-17"},
-                {"pi()*3/4", "-0.7071067811865475"}, {"pi()", "-1.0"}};
-    }
-
-    @DataProvider
     public static Object[][] provideAcosParameters() {
         return new Object[][]{{"-1", "3.141592653589793"}, {"-1/2", "2.0943951023931957"}, {"0", "1.5707963267948966"},
                 {"1/2", "1.0471975511965979"}, {"1", "0.0"}};
-    }
-
-    @DataProvider
-    public static Object[][] provideCotParameters() {
-        return new Object[][]{{"-pi()*3/4", "0.9999999999999998"},
-                {"-pi()/2", "-6.123233995736766E-17"}, {"-pi()/4", "-1.0000000000000002"},
-                {"pi()/4", "1.0000000000000002"}, {"pi()/2", "6.123233995736766E-17"},
-                {"pi()*3/4", "-0.9999999999999998"}};
-    }
-
-    @DataProvider
-    public static Object[][] provideTanParameters() {
-        return new Object[][]{{"-pi()", "1.2246467991473532E-16"}, {"-pi()*3/4", "1.0000000000000002"},
-                {"-pi()/4", "-0.9999999999999999"}, {"0", "0.0"}, {"pi()/4", "0.9999999999999999"},
-                {"pi()*3/4", "-1.0000000000000002"}, {"pi()", "-1.2246467991473532E-16"}};
     }
 
     @DataProvider
@@ -70,16 +87,16 @@ public class SqlTrigonometricFunctionTest extends SqlTest {
     }
 
     @Issue("5764")
-    @Test(dataProvider = "provideSinParameters")
-    public void testSin(String functionParameter, String expectedResult) {
-        String sqlQuery = String.format("SELECT SIN(%s)", functionParameter);
+    @Test(dataProvider = "provideTrigonometricValues")
+    public void testTrigonometricFunctions(String functionParameter, Double expectedResult, TrigonometricAccessor accessor) {
+        String sqlQuery = String.format("SELECT %s(%s)", accessor.functionName, functionParameter);
 
         StringTable resultTable = queryResponse(sqlQuery).readEntity(StringTable.class);
 
         Assert.assertEquals(resultTable.getTableMetaData().getColumnMeta(0).getDataType(), "double",
-                "Sin(" + functionParameter + ") wrong data type");
-        Assert.assertEquals(resultTable.getRows().get(0).get(0), expectedResult,
-                "Sin(" + functionParameter + ") wrong result");
+                accessor.functionName + "(" + functionParameter + ") wrong data type");
+        Assert.assertEquals(Double.valueOf(resultTable.getRows().get(0).get(0)), accessor.apply(expectedResult), DELTA,
+                accessor.functionName + "(" + functionParameter + ") wrong result");
     }
 
     @Issue("5764")
@@ -90,22 +107,9 @@ public class SqlTrigonometricFunctionTest extends SqlTest {
         StringTable resultTable = queryResponse(sqlQuery).readEntity(StringTable.class);
 
         Assert.assertEquals(resultTable.getTableMetaData().getColumnMeta(0).getDataType(), "double",
-                "Sin(" + functionParameter + ") wrong data type");
+                "Asin(" + functionParameter + ") wrong data type");
         Assert.assertEquals(resultTable.getRows().get(0).get(0), expectedResult,
-                "Sin(" + functionParameter + ") wrong result");
-    }
-
-    @Issue("5764")
-    @Test(dataProvider = "provideCosParameters")
-    public void testCos(String functionParameter, String expectedResult) {
-        String sqlQuery = String.format("SELECT COS(%s)", functionParameter);
-
-        StringTable resultTable = queryResponse(sqlQuery).readEntity(StringTable.class);
-
-        Assert.assertEquals(resultTable.getTableMetaData().getColumnMeta(0).getDataType(), "double",
-                "Cos(" + functionParameter + ") wrong data type");
-        Assert.assertEquals(resultTable.getRows().get(0).get(0), expectedResult,
-                "Cos(" + functionParameter + ") wrong result");
+                "Asin(" + functionParameter + ") wrong result");
     }
 
     @Issue("5764")
@@ -116,35 +120,9 @@ public class SqlTrigonometricFunctionTest extends SqlTest {
         StringTable resultTable = queryResponse(sqlQuery).readEntity(StringTable.class);
 
         Assert.assertEquals(resultTable.getTableMetaData().getColumnMeta(0).getDataType(), "double",
-                "Cos(" + functionParameter + ") wrong data type");
+                "Acos(" + functionParameter + ") wrong data type");
         Assert.assertEquals(resultTable.getRows().get(0).get(0), expectedResult,
-                "Cos(" + functionParameter + ") wrong result");
-    }
-
-    @Issue("5764")
-    @Test(dataProvider = "provideCotParameters")
-    public void testCot(String functionParameter, String expectedResult) {
-        String sqlQuery = String.format("SELECT COT(%s)", functionParameter);
-
-        StringTable resultTable = queryResponse(sqlQuery).readEntity(StringTable.class);
-
-        Assert.assertEquals(resultTable.getTableMetaData().getColumnMeta(0).getDataType(), "double",
-                "Cot(" + functionParameter + ") wrong data type");
-        Assert.assertEquals(resultTable.getRows().get(0).get(0), expectedResult,
-                "Cot(" + functionParameter + ") wrong result");
-    }
-
-    @Issue("5764")
-    @Test(dataProvider = "provideTanParameters")
-    public void testTan(String functionParameter, String expectedResult) {
-        String sqlQuery = String.format("SELECT TAN(%s)", functionParameter);
-
-        StringTable resultTable = queryResponse(sqlQuery).readEntity(StringTable.class);
-
-        Assert.assertEquals(resultTable.getTableMetaData().getColumnMeta(0).getDataType(), "double",
-                "Tan(" + functionParameter + ") wrong data type");
-        Assert.assertEquals(resultTable.getRows().get(0).get(0), expectedResult,
-                "Tan(" + functionParameter + ") wrong result");
+                "Acos(" + functionParameter + ") wrong result");
     }
 
     @Issue("5764")
@@ -155,9 +133,9 @@ public class SqlTrigonometricFunctionTest extends SqlTest {
         StringTable resultTable = queryResponse(sqlQuery).readEntity(StringTable.class);
 
         Assert.assertEquals(resultTable.getTableMetaData().getColumnMeta(0).getDataType(), "double",
-                "Tan(" + functionParameter + ") wrong data type");
+                "Atan(" + functionParameter + ") wrong data type");
         Assert.assertEquals(resultTable.getRows().get(0).get(0), expectedResult,
-                "Tan(" + functionParameter + ") wrong result");
+                "Atan(" + functionParameter + ") wrong result");
     }
 
     @Issue("5764")
