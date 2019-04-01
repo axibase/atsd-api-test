e307d4e327a3f18a85c269aa0d2c6b47e2890bce
VVVyacheslav <addams_addams@mail.ru>
2019-04-01 16:44:30 +0300

    Remarks is eliminated. Added separation on sets of output series

--- src/test/java/com/axibase/tsd/api/method/series/SeriesQueryTransformationWithDifferentForecast.java
+++ src/test/java/com/axibase/tsd/api/method/series/SeriesQueryTransformationWithDifferentForecast.java
@@ -35,64 +35,100 @@ import static org.testng.Assert.assertEquals;
  * Take series transformations {@link Transformation#values()} with multiple sets of output series (@link Transformation#AGGREGATE,
  * @link Transformation#GROUP, @link Transformation#Forcast) with different state of parameter DETAIL
  * Check that response contains correct number of generated series for each permutation of the transformations.
+ *
+ * Methods insertSeries() and addSamplesToSeries() create input series
+ *
+ * Methods setUpAggregation(), setUpGrouping(), and setUpForecasting() create test set of queries
  */
 
 
 public class SeriesQueryTransformationWithDifferentForecast extends SeriesMethod {
-    private int inputSeriesCount;
-    private int queryCount;
+    /**
+     * Number of transform series
+     */
+    private static final int INPUT_SERIES_COUNT = 5;
+
+    /**
+     * Parameters of compilation of series
+     */
+    private static final int TIME_INTERVAL = 1;
+    private static final String START_DATE = "2019-01-01T00:00:00Z";
+    private static final String END_DATE = "2019-01-02T00:00:00Z";
+    private static final int SERIES_VALUE = 101;
+    private static final int SECONDS_IN_HALF_MINUTE = 30;
+    private static final int HALF_MINETES = 2;
+    private static final String ZONE_ID = "Etc/UTC";
+    private static final String TAG_NAME = "tag-name-1";
+    private static final String TAG_VALUE = "tag-value-1";
+    private static final String QUERY_ENTITY = "*";
+
+    /**
+     * Parameters of forecasting algorithms
+     */
+    private static final int HORIZON_LENGTH = 100;
+    private static final double ALPHA = 0.5;
+    private static final double BETTA = 0.5;
+    private static final double GAMMA = 0.5;
+    private static final int EIGENTRIPLE_LIMIT = 100;
+    private static final int SINGULAR_VALUE_THRESHOLD = 5;
+
+    /**
+     * Period count for aggregate and group, respectively
+     */
+    private static final int AGGREGATION_PERIOD_COUNT = 3;
+    private static final int GROUP_PERIOD_COUNT = 5;
+
+    /**
+     * Expected error
+     */
+    private static final int STATUS_ERROR = 400;
+
+    /**
+     * Set of transformation and created queries
+     */
     List<SeriesQuery> queryList = new ArrayList<>();
-
-
-
-    int days = 1;
-    String startDate = "2019-01-01T00:00:00Z";
-    String endDate = "2019-01-02T00:00:00Z";
-
     List<Transformation> transformations = Arrays.asList(
             Transformation.AGGREGATE,
             Transformation.FORECAST,
             Transformation.GROUP);
-    Aggregate aggregationSettings;
-    Group groupSettings;
-    Forecast forecastSettings;
 
     @BeforeClass
-    public void prepareData() throws Exception {
+    private void prepareData() throws Exception {
         String metric = Mocks.metric();
         insertSeries(metric);
-        queryCount = 12;
 
-        for (int i=0; i<queryCount; i++) {
-            queryList.add(new SeriesQuery( "*", metric, startDate, endDate));
+        List<Aggregate> aggregates = setUpAggregation();
+        List<Group> groups = setUpGrouping();
+        List<Forecast> forecasts = setUpForecasting();
+
+        for (Aggregate aggregate: aggregates) {
+            for (Group group: groups) {
+                for (Forecast forecast: forecasts) {
+                    queryList.add(new SeriesQuery(QUERY_ENTITY, metric, START_DATE, END_DATE).setAggregate(aggregate).setGroup(group).setForecast(forecast));
+                }
+            }
         }
-
-        setUpAggregation();
-        setUpGrouping();
-        setUpForecasting();
     }
 
     private void insertSeries(String metric) throws Exception {
-
         String entity1 = Mocks.entity();
         String entity2 = Mocks.entity();
         String entity3 = Mocks.entity();
 
         Series series1 = new Series(entity1, metric);
-        Series series2 = new Series(entity1, metric, "tag-name-1", "tag-value-1");
-        Series series3 = new Series(entity2, metric, "tag-name-1", "tag-value-1");
-        Series series4 = new Series(entity2, metric, "tag-name-1", "tag-value-2");
+        Series series2 = new Series(entity1, metric, TAG_NAME, TAG_VALUE);
+        Series series3 = new Series(entity2, metric, TAG_NAME, TAG_VALUE);
+        Series series4 = new Series(entity2, metric, TAG_NAME, TAG_VALUE);
         Series series5 = new Series(entity3, metric);
         addSamplesToSeries(series1, series2, series3, series4, series5);
         insertSeriesCheck(series1, series2, series3, series4, series5);
-        inputSeriesCount = 5;
     }
 
     private void addSamplesToSeries(Series series1, Series series2, Series series3, Series series4, Series series5) {
-        int totalSamplesCount = days * 24 * 60 * 2;
+        long totalSamplesCount = TIME_INTERVAL * java.util.concurrent.TimeUnit.DAYS.toMinutes(1) * HALF_MINETES;
         for (int i = 0; i < totalSamplesCount; i++) {
-            String time = TestUtil.addTimeUnitsInTimezone(startDate, ZoneId.of("Etc/UTC"), TimeUnit.SECOND, 30 * i);
-            Sample sample = Sample.ofDateInteger(time, 101);
+            String time = TestUtil.addTimeUnitsInTimezone(START_DATE, ZoneId.of(ZONE_ID), TimeUnit.SECOND, SECONDS_IN_HALF_MINUTE * i);
+            Sample sample = Sample.ofDateInteger(time, SERIES_VALUE);
             series1.addSamples(sample);
             series2.addSamples(sample);
             series3.addSamples(sample);
@@ -101,81 +137,70 @@ public class SeriesQueryTransformationWithDifferentForecast extends SeriesMethod
         }
     }
 
-    private void setUpAggregation() {
-        Period period = new Period(3, TimeUnit.MINUTE, PeriodAlignment.START_TIME);
+    private List<Aggregate> setUpAggregation() {
+        List<Aggregate> aggregates = new ArrayList<>();
+        List<List<AggregationType>> setsAggregationType = Arrays.asList(Arrays.asList(AggregationType.AVG, AggregationType.SUM, AggregationType.FIRST),
+                Arrays.asList(AggregationType.AVG, AggregationType.SUM, AggregationType.FIRST, AggregationType.DETAIL));
+        Period period = new Period(AGGREGATION_PERIOD_COUNT, TimeUnit.MINUTE, PeriodAlignment.START_TIME);
         AggregationInterpolate interp = new AggregationInterpolate(AggregationInterpolateType.LINEAR, true);
 
-        aggregationSettings = new Aggregate()
-                .setPeriod(period)
-                .setTypes(Arrays.asList(AggregationType.AVG, AggregationType.SUM, AggregationType.FIRST))
-                .setInterpolate(interp);
-        for (int i=0; i<queryCount/2; i++) {
-                queryList.get(i).setAggregate(aggregationSettings);
+        for (List<AggregationType> setAggregationType: setsAggregationType) {
+            aggregates.add(new Aggregate()
+                    .setPeriod(period)
+                    .setInterpolate(interp)
+                    .setTypes(setAggregationType));
         }
 
-        aggregationSettings = new Aggregate()
-                .setPeriod(period)
-                .setTypes(Arrays.asList(AggregationType.AVG, AggregationType.SUM, AggregationType.FIRST, AggregationType.DETAIL))
-                .setInterpolate(interp);
-        for (int i=queryCount/2; i<queryCount; i++) {
-                queryList.get(i).setAggregate(aggregationSettings);
-        }
+        return aggregates;
     }
 
-    private void setUpGrouping() {
-        Period period = new Period(5, TimeUnit.MINUTE, PeriodAlignment.START_TIME);
+    private List<Group> setUpGrouping() {
+        List<Group> groups = new ArrayList<>();
+        List<List<GroupType>> setsGroupType = Arrays.asList(Arrays.asList(GroupType.AVG, GroupType.SUM),
+                Arrays.asList(GroupType.AVG, GroupType.SUM, GroupType.DETAIL));
+        Period period = new Period(GROUP_PERIOD_COUNT, TimeUnit.MINUTE, PeriodAlignment.START_TIME);
         AggregationInterpolate interp = new AggregationInterpolate(AggregationInterpolateType.LINEAR, true);
 
-        groupSettings = new Group()
-                .setPeriod(period)
-                .setTypes(Arrays.asList(GroupType.AVG, GroupType.SUM))
-                .setInterpolate(interp);
-        for (int i=0; i<queryCount; i+=2) {
-                queryList.get(i).setGroup(groupSettings);
+        for (List<GroupType> setGroupType: setsGroupType) {
+            groups.add(new Group()
+                    .setPeriod(period)
+                    .setInterpolate(interp)
+                    .setTypes(setGroupType));
         }
 
-        groupSettings = new Group()
-                .setPeriod(period)
-                .setTypes(Arrays.asList(GroupType.AVG, GroupType.SUM, GroupType.DETAIL))
-                .setInterpolate(interp);
-        for (int i=1; i<queryCount; i+=2) {
-                queryList.get(i).setGroup(groupSettings);
-        }
+        return groups;
     }
 
-    private void setUpForecasting() {
-        Horizon horizon = new Horizon().setLength(100);
-        HoltWintersSettings holtWintersSettings = new HoltWintersSettings().setAlpha(0.5).setBeta(0.5).setGamma(0.5).setAuto(false);
-        SSASettings ssaSettings = new SSASettings().setDecompose(new SSADecompositionSettings().setMethod(SvdMethod.AUTO).setEigentripleLimit(100).setSingularValueThreshold(5));
-//        ARIMASettings arimaSettings = new ARIMASettings();
+    private List<Forecast> setUpForecasting() {
+        List<Forecast> forecasts = new ArrayList<>();
+        List<List<SeriesType>> setsSeriesType = Arrays.asList(Arrays.asList(SeriesType.FORECAST, SeriesType.RECONSTRUCTED),
+                Arrays.asList(SeriesType.FORECAST, SeriesType.RECONSTRUCTED, SeriesType.HISTORY));
+        Horizon horizon = new Horizon().setLength(HORIZON_LENGTH);
+        HoltWintersSettings holtWintersSettings = new HoltWintersSettings().setAlpha(ALPHA).setBeta(BETTA).setGamma(GAMMA).setAuto(false);
+        SSASettings ssaSettings = new SSASettings().setDecompose(new SSADecompositionSettings().setMethod(SvdMethod.AUTO).setEigentripleLimit(EIGENTRIPLE_LIMIT).setSingularValueThreshold(SINGULAR_VALUE_THRESHOLD));
 
-        forecastSettings = new Forecast()
-                .setHw(holtWintersSettings)
-                .setHorizon(horizon)
-                .setInclude(Arrays.asList(SeriesType.FORECAST, SeriesType.HISTORY, SeriesType.RECONSTRUCTED));
-        for (int i=0; i<queryCount; i+=3) {
-                queryList.get(i).setForecast(forecastSettings);
+        for (List<SeriesType> setSeriesType: setsSeriesType) {
+            forecasts.add(new Forecast()
+                    .setHw(holtWintersSettings)
+                    .setHorizon(horizon)
+                    .setInclude(setSeriesType));
+
+            forecasts.add(new Forecast()
+                    .setSsa(ssaSettings)
+                    .setHorizon(horizon)
+                    .setInclude(setSeriesType));
+
+            forecasts.add(new Forecast()
+                    .setHw(holtWintersSettings)
+                    .setSsa(ssaSettings)
+                    .setHorizon(horizon)
+                    .setInclude(setSeriesType));
         }
 
-        forecastSettings = new Forecast()
-                .setSsa(ssaSettings)
-                .setHorizon(horizon)
-                .setInclude(Arrays.asList(SeriesType.FORECAST, SeriesType.HISTORY, SeriesType.RECONSTRUCTED));
-        for (int i=1; i<queryCount; i+=3) {
-            queryList.get(i).setForecast(forecastSettings);
-        }
-
-        forecastSettings = new Forecast()
-                .setHw(holtWintersSettings)
-                .setSsa(ssaSettings)
-                .setHorizon(horizon)
-                .setInclude(Arrays.asList(SeriesType.FORECAST, SeriesType.HISTORY, SeriesType.RECONSTRUCTED));
-        for (int i=2; i<queryCount; i+=3) {
-            queryList.get(i).setForecast(forecastSettings);
-        }
+        return forecasts;
     }
 
-    @DataProvider(name = "permutations"/*, parallel = true*/)
+    @DataProvider(name = "permutations", parallel = true)
     Object[][] permuteTransformations() {
 
         int permutationsCount = (int) CombinatoricsUtils.factorial(transformations.size());
@@ -200,11 +225,11 @@ public class SeriesQueryTransformationWithDifferentForecast extends SeriesMethod
             description = "Take series transformations {@link Transformation#values()} with multiple set of output series. " +
                     "Create query which has these transformations with different status of parameter DETAIL in AGGREGATE and GROUP " +
                     "Check that response contains correct number of generated series for each permutation of the transformations.")
-    public void Test(List<Transformation> permutation, SeriesQuery query) {
+    public void testDifferentOrderTransformations(List<Transformation> permutation, SeriesQuery query) {
         query.setTransformationOrder(permutation);
 
         if (irregularSeriesForForecastException(permutation, query)) {
-            assertEquals(querySeries(query).getStatus(), 400);
+            assertEquals(querySeries(query).getStatus(), STATUS_ERROR);
         } else {
             List<Series> seriesList = querySeriesAsList(query);
             int expectedSeriesCount = countExpectedSeries(permutation, query);
@@ -213,15 +238,15 @@ public class SeriesQueryTransformationWithDifferentForecast extends SeriesMethod
     }
 
     private int countExpectedSeries(List<Transformation> permutation, SeriesQuery query) {
-        int seriesCount = inputSeriesCount;
-        for (int i=0; i<permutation.size(); i++) {
-            switch (permutation.get(i)) {
+        int seriesCount = INPUT_SERIES_COUNT;
+        for (Transformation transformation: permutation) {
+            switch (transformation) {
                 case GROUP:
                     int groupingFunctionsCount = query.getGroup().getTypes().size();
                     if (query.getGroup().getTypes().contains(GroupType.DETAIL)) {
-                        seriesCount += (seriesCount / inputSeriesCount) * (groupingFunctionsCount-1);
+                        seriesCount += (seriesCount / INPUT_SERIES_COUNT) * (groupingFunctionsCount-1);
                     } else {
-                        seriesCount = (seriesCount / inputSeriesCount) * groupingFunctionsCount;
+                        seriesCount = (seriesCount / INPUT_SERIES_COUNT) * groupingFunctionsCount;
                     }
                     break;
                 case AGGREGATE:
