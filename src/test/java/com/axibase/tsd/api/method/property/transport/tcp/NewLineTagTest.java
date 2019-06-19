2135b079c573d0d3ff0a78d51ed321e8ee677cf3
Alexander Kostyurenko <alekkostyur@gmail.com>
2019-06-19 14:51:50 +0300

    Added test for property tags after tag with new line

---
+++ src/test/java/com/axibase/tsd/api/method/property/transport/tcp/NewLineTagTest.java
@@ -0,0 +1,48 @@
+package com.axibase.tsd.api.method.property.transport.tcp;
+
+import com.axibase.tsd.api.method.property.PropertyTest;
+import com.axibase.tsd.api.model.command.PlainCommand;
+import com.axibase.tsd.api.model.command.PropertyCommand;
+import com.axibase.tsd.api.model.property.Property;
+import com.axibase.tsd.api.transport.tcp.TCPSender;
+import com.axibase.tsd.api.util.Mocks;
+import com.google.common.collect.ImmutableMap;
+import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+import static org.testng.AssertJUnit.*;
+
+public class NewLineTagTest extends PropertyTest {
+
+    @DataProvider
+    private Object[][] newLines() {
+        return new String[][] {
+                {"\n"},
+                {" \n"},
+                {"\n "},
+                {"\r\n"},
+                {"\n\r"}
+        };
+    }
+
+    @Issue("6327")
+    @Description("Test that tags that comes after tag with new line")
+    @Test(
+            dataProvider = "newLines"
+    )
+    public void testTagsAfterNewLine(String newLine) throws Exception {
+        String propertyType = Mocks.propertyType();
+        String entityName = Mocks.entity();
+        Property property = new Property(propertyType, entityName);
+        property.setTags(ImmutableMap.of("t1", newLine, "t2", "v2"));
+        PlainCommand command = new PropertyCommand(property);
+        String response = TCPSender.send(command, true);
+        assertEquals("Property command with newline tag did not returned ok", "ok", response);
+        property.setTags(ImmutableMap.of("t2", "v2"));
+        assertPropertyExisting(property);
+    }
+
+}
