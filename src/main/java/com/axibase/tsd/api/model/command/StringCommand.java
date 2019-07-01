ce033a489ed2f2cbc7c2d0e901d5693a050af0c2
Alexander Kostyurenko <alekkostyur@gmail.com>
2019-07-01 16:35:05 +0300

    Refactored StringCommand

--- src/main/java/com/axibase/tsd/api/model/command/StringCommand.java
+++ src/main/java/com/axibase/tsd/api/model/command/StringCommand.java
@@ -4,15 +4,13 @@ package com.axibase.tsd.api.model.command;
  * Class that transforms string command to PlainCommand
  */
 public class StringCommand extends AbstractCommand {
-    private final String command;
 
     public StringCommand(String command) {
-        super("");
-        this.command = command;
+        super(command);
     }
 
     @Override
     public String compose() {
-        return command;
+        return commandBuilder().toString();
     }
 }
