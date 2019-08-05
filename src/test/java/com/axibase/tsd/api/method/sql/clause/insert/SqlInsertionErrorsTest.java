bb031bdf12f17b2e22caa89db4802c5789c8d777
Alexander Kostyurenko <alekkostyur@gmail.com>
2019-08-05 11:54:22 +0300

    Updated error message extraction

--- src/test/java/com/axibase/tsd/api/method/sql/clause/insert/SqlInsertionErrorsTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/clause/insert/SqlInsertionErrorsTest.java
@@ -24,14 +24,89 @@ public class SqlInsertionErrorsTest extends SqlTest {
     @Issue("5962")
     public void testUnsetDeclaredParameterInsertion() {
         String sqlQuery;
+        String assertMessage;
         if(insertionType == InsertionType.INSERT_INTO) { //cannot create ImmutableMap with unset parameter
             sqlQuery = String.format("INSERT INTO \"%s\"(entity, datetime, value, not_set) VALUES('%s', '%s', %d)"
                     , Mocks.metric(), Mocks.entity(), ISO_TIME, VALUE);
+            assertMessage = "IllegalArgumentException: No value specified for column 'not_set'";
         } else {
             sqlQuery = String.format("UPDATE \"%s\" SET entity='%s', datetime='%s', value=%d, not_set="
                     , Mocks.metric(), Mocks.entity(), ISO_TIME, VALUE);
+            assertMessage = "net.sf.jsqlparser.parser.ParseException: Encountered unexpected token:<EOF>\n" + //assert messages are different for two clauses
+                    "    at line 1, column 326.\n" +
+                    "\n" +
+                    "Was expecting one of:\n" +
+                    "\n" +
+                    "    \"+\"\n" +
+                    "    \"-\"\n" +
+                    "    \"?\"\n" +
+                    "    \"@\"\n" +
+                    "    \"@@\"\n" +
+                    "    \"ACTION\"\n" +
+                    "    \"ANY\"\n" +
+                    "    \"BYTE\"\n" +
+                    "    \"CASCADE\"\n" +
+                    "    \"CASE\"\n" +
+                    "    \"CAST\"\n" +
+                    "    \"CHAR\"\n" +
+                    "    \"COLUMN\"\n" +
+                    "    \"COLUMNS\"\n" +
+                    "    \"COMMENT\"\n" +
+                    "    \"COMMIT\"\n" +
+                    "    \"DO\"\n" +
+                    "    \"ENABLE\"\n" +
+                    "    \"END\"\n" +
+                    "    \"EXTRACT\"\n" +
+                    "    \"FIRST\"\n" +
+                    "    \"FOLLOWING\"\n" +
+                    "    \"GROUP_CONCAT\"\n" +
+                    "    \"INDEX\"\n" +
+                    "    \"INSERT\"\n" +
+                    "    \"INTERVAL\"\n" +
+                    "    \"ISNULL\"\n" +
+                    "    \"KEY\"\n" +
+                    "    \"LAST\"\n" +
+                    "    \"MATERIALIZED\"\n" +
+                    "    \"NO\"\n" +
+                    "    \"NULL\"\n" +
+                    "    \"NULLS\"\n" +
+                    "    \"OPEN\"\n" +
+                    "    \"OVER\"\n" +
+                    "    \"PARTITION\"\n" +
+                    "    \"PERCENT\"\n" +
+                    "    \"PRECISION\"\n" +
+                    "    \"PRIMARY\"\n" +
+                    "    \"PRIOR\"\n" +
+                    "    \"RANGE\"\n" +
+                    "    \"REPLACE\"\n" +
+                    "    \"ROW\"\n" +
+                    "    \"ROWS\"\n" +
+                    "    \"SEPARATOR\"\n" +
+                    "    \"SIBLINGS\"\n" +
+                    "    \"TABLE\"\n" +
+                    "    \"TEMP\"\n" +
+                    "    \"TEMPORARY\"\n" +
+                    "    \"TRUNCATE\"\n" +
+                    "    \"TYPE\"\n" +
+                    "    \"UNSIGNED\"\n" +
+                    "    \"VALUE\"\n" +
+                    "    \"VALUES\"\n" +
+                    "    \"XML\"\n" +
+                    "    \"ZONE\"\n" +
+                    "    \"{d\"\n" +
+                    "    \"{t\"\n" +
+                    "    \"{ts\"\n" +
+                    "    \"~\"\n" +
+                    "    <K_DATETIMELITERAL>\n" +
+                    "    <K_TIME_KEY_EXPR>\n" +
+                    "    <S_CHAR_LITERAL>\n" +
+                    "    <S_DOUBLE>\n" +
+                    "    <S_HEX>\n" +
+                    "    <S_IDENTIFIER>\n" +
+                    "    <S_LONG>\n" +
+                    "    <S_QUOTED_IDENTIFIER>\n";
         }
-        assertBadSqlRequest("Invalid SQL query with unset declared parameter was accepted!", sqlQuery);
+        assertBadRequest("Invalid SQL query with unset declared parameter was accepted!", assertMessage, sqlQuery);
     }
 
     @Test(
@@ -41,6 +116,6 @@ public class SqlInsertionErrorsTest extends SqlTest {
     public void testUnsetRequiredParameterInsertion() {
         String sqlQuery = insertionType.insertionQuery(Mocks.metric(),
                 ImmutableMap.of("entity", Mocks.entity(), "datetime", ISO_TIME)); //value is not set
-        assertBadSqlRequest("Invalid SQL query with unset required parameter was accepted!", sqlQuery);
+        assertBadRequest("Invalid SQL query with unset required parameter was accepted!", "IllegalArgumentException: Either value or text is required",sqlQuery);
     }
 }
