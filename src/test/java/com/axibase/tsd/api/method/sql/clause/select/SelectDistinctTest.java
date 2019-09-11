4d13ff47cf08abb4331b0339429e32ab221ed899
Anton Rybochkin <anton.rybochkin@axibase.com>
2019-09-11 20:17:57 +0300

    style fixes

--- src/test/java/com/axibase/tsd/api/method/sql/clause/select/SelectDistinctTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/clause/select/SelectDistinctTest.java
@@ -6,67 +6,69 @@ import com.axibase.tsd.api.model.series.Sample;
 import com.axibase.tsd.api.model.series.Series;
 import com.axibase.tsd.api.util.Mocks;
 import io.qameta.allure.Issue;
+import lombok.Data;
+import org.apache.commons.lang3.ArrayUtils;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class SelectDistinctTest extends SqlTest {
     private static final String METRIC = Mocks.metric();
 
-    private static final String ENTITY_1 = Mocks.entity();
-    private static final String ENTITY_2 = Mocks.entity();
-    private static final String ENTITY_3 = Mocks.entity();
-
-    private static final int[] DISTINCT_DATA = {1, 2, 3};
-    private static final long[] DISTINCT_TIME = {Mocks.MILLS_TIME + 1, Mocks.MILLS_TIME  + 2, Mocks.MILLS_TIME + 3};
+    private static final List<DistinctSample> DISTINCT_SAMPLES = Arrays.asList(
+            DistinctSample.of(Mocks.entity(), Mocks.MILLS_TIME + 1, 1),
+            DistinctSample.of(Mocks.entity(), Mocks.MILLS_TIME + 2, 2),
+            DistinctSample.of(Mocks.entity(), Mocks.MILLS_TIME + 3, 3)
+    );
 
     @BeforeClass
-    public void prepareData() throws Exception {
-        Series series1 = new Series(ENTITY_1, METRIC);
-        Series series2 = new Series().setEntity(ENTITY_2).setMetric(METRIC);
-        Series series3 = new Series().setEntity(ENTITY_3).setMetric(METRIC);
-
-        for(int i = 0; i < DISTINCT_DATA.length; i++) {
-            Sample sample = Sample.ofTimeInteger(DISTINCT_TIME[i], DISTINCT_DATA[i]);
-            series1.addSamples(sample);
-            series2.addSamples(sample);
-            series3.addSamples(sample);
-        }
-        SeriesMethod.insertSeriesCheck(series1, series2, series3);
+    public static void prepareData() throws Exception {
+        final List<Series> samples = DISTINCT_SAMPLES.stream()
+                .map(s -> new Series()
+                        .setMetric(METRIC)
+                        .setEntity(s.entity)
+                        .addSamples(Sample.ofTimeInteger(s.timestamp, s.value)))
+                .collect(Collectors.toList());
+        SeriesMethod.insertSeriesCheck(samples);
     }
 
     @Issue("6536")
     @Test(description = "Tests that 'SELECT DISTINCT entity' returns unique entities")
     public void testSelectDistinctEntity() {
         String sqlQuery = String.format("SELECT DISTINCT entity FROM \"%s\" ORDER BY entity", METRIC);
-        String[][] expectedRows = {
-                {ENTITY_1},
-                {ENTITY_2},
-                {ENTITY_3}
-        };
-        assertSqlQueryRows(expectedRows, sqlQuery);
+        assertSqlQueryRows(composeExpectedRows(DistinctSample::getEntity), sqlQuery);
     }
 
     @Issue("6536")
     @Test(description = "Tests that 'SELECT DISTINCT value' returns unique values")
     public void testSelectDistinctValue() {
         String sqlQuery = String.format("SELECT DISTINCT value FROM \"%s\" ORDER BY value", METRIC);
-        String[][] expectedRows = {
-                {String.valueOf(DISTINCT_DATA[0])},
-                {String.valueOf(DISTINCT_DATA[1])},
-                {String.valueOf(DISTINCT_DATA[2])}
-        };
-        assertSqlQueryRows(expectedRows, sqlQuery);
+        assertSqlQueryRows(composeExpectedRows(DistinctSample::getValue), sqlQuery);
     }
 
     @Issue("6536")
     @Test(description = "Tests that 'SELECT DISTINCT time' returns unique values")
     public void testSelectDistinctTime() {
         String sqlQuery = String.format("SELECT DISTINCT time FROM \"%s\" ORDER BY time", METRIC);
-        String[][] expectedRows = {
-                {String.valueOf(DISTINCT_TIME[0])},
-                {String.valueOf(DISTINCT_TIME[1])},
-                {String.valueOf(DISTINCT_TIME[2])}
-        };
-        assertSqlQueryRows(expectedRows, sqlQuery);
+        assertSqlQueryRows(composeExpectedRows(DistinctSample::getTimestamp), sqlQuery);
+    }
+
+    private static String[][] composeExpectedRows(Function<DistinctSample, Object> fieldGetter) {
+        return DISTINCT_SAMPLES.stream()
+                .map(fieldGetter)
+                .map(String::valueOf)
+                .map(ArrayUtils::toArray)
+                .toArray(String[][]::new);
+    }
+
+    @Data(staticConstructor = "of")
+    private static final class DistinctSample {
+        private final String entity;
+        private final long timestamp;
+        private final int value;
     }
 }
