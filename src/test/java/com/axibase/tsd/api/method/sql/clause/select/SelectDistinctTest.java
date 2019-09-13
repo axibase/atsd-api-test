77152041124f75a0d001faee1302b54e0d8ff8a7
Alexander Kostyurenko <alekkostyur@gmail.com>
2019-09-13 17:07:49 +0300

    Added tests for selecting distinct data from columns with equal values

--- src/test/java/com/axibase/tsd/api/method/sql/clause/select/SelectDistinctTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/clause/select/SelectDistinctTest.java
@@ -25,6 +25,13 @@ public class SelectDistinctTest extends SqlTest {
             DistinctSample.of(Mocks.entity(), Mocks.MILLS_TIME + 3, 3)
     );
 
+    private static final String METRIC_2 = Mocks.metric();
+    private static final List<DistinctSample> EQUAL_DISTINCT_SAMPLES = Arrays.asList(
+            DistinctSample.of("6536", 6536, 6536),
+            DistinctSample.of("6537", 6537, 6537),
+            DistinctSample.of("6538", 6538, 6538)
+    );
+
     @BeforeClass
     public static void prepareData() throws Exception {
         final List<Series> samples = DISTINCT_SAMPLES.stream()
@@ -34,6 +41,14 @@ public class SelectDistinctTest extends SqlTest {
                         .addSamples(Sample.ofTimeInteger(s.timestamp, s.value)))
                 .collect(Collectors.toList());
         SeriesMethod.insertSeriesCheck(samples);
+
+        final List<Series> equalSamples = EQUAL_DISTINCT_SAMPLES .stream()
+                .map(s -> new Series()
+                            .setMetric(METRIC_2)
+                            .setEntity(s.entity)
+                            .addSamples(Sample.ofTimeInteger(s.timestamp, s.value)))
+                .collect(Collectors.toList());
+        SeriesMethod.insertSeriesCheck(equalSamples);
     }
 
     @Issue("6536")
@@ -66,10 +81,23 @@ public class SelectDistinctTest extends SqlTest {
         ), sqlQuery);
     }
 
-    private static String[][] composeExpectedRowsFromStringArray(Function<DistinctSample, String[]> arrayComposer) {
-        return DISTINCT_SAMPLES.stream()
+    @Issue("6536")
+    @Test(description = "Tests that 'SELECT DISTINCT' returns correct data if all columns have equal values")
+    public void testSelectDistinctEqualData() {
+        String sqlQuery = String.format("SELECT DISTINCT entity, value, time FROM \"%s\"", METRIC_2);
+        assertSqlQueryRows(composeExpectedRowsFromStringArray(EQUAL_DISTINCT_SAMPLES, sample ->
+                new String[] {sample.getEntity(), String.valueOf(sample.getValue()), String.valueOf(sample.getTimestamp())}
+        ), sqlQuery);
+    }
+
+    private static String[][] composeExpectedRowsFromStringArray(List<DistinctSample> sampleList,Function<DistinctSample, String[]> arrayComposer) {
+        return sampleList.stream()
                 .map(arrayComposer)
                 .toArray(String[][]::new);
+    }
+
+    private static String[][] composeExpectedRowsFromStringArray(Function<DistinctSample, String[]> arrayComposer) {
+        return composeExpectedRowsFromStringArray(DISTINCT_SAMPLES, arrayComposer);
     }
 
     private static String[][] composeExpectedRows(Function<DistinctSample, Object> fieldGetter) {
