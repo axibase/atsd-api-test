690351667ddcfb5a7e78ae928b1680fb1e0cc7ee
alexandertokarev <atokarev@gmail.com>
2020-06-15 17:46:58 +0300

    Tests for trade property functions

---
+++ src/test/java/com/axibase/tsd/api/method/sql/trade/TradePropertyTest.java
@@ -0,0 +1,79 @@
+package com.axibase.tsd.api.method.sql.trade;
+
+import com.axibase.tsd.api.Checker;
+import com.axibase.tsd.api.method.checks.EntityCheck;
+import com.axibase.tsd.api.method.property.PropertyMethod;
+import com.axibase.tsd.api.model.entity.Entity;
+import com.axibase.tsd.api.model.financial.Trade;
+import com.axibase.tsd.api.model.property.Property;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TradePropertyTest extends SqlTradeTest {
+    public static final String STATISTICS = "statistics";
+    public static final String SECURITY_DEFINITIONS = "security_definitions";
+
+    @BeforeClass
+    public void prepareData() throws Exception {
+        Trade trade = fromISOString("2020-06-15T10:21:49.123456Z");
+        insert(trade);
+        Entity entity = new Entity(entity());
+        EntityCheck entityCheck = new EntityCheck(entity);
+        Checker.check(entityCheck);
+        Property property = new Property(SECURITY_DEFINITIONS, entity.getName());
+        property.setDate("2020-06-14T10:21:49.123Z");
+        Map<String, String> tags = new HashMap<>();
+        tags.put("roundlot", "10");
+        tags.put("product", "5");
+        property.setTags(tags);
+        PropertyMethod.insertPropertyCheck(property);
+        property = new Property(SECURITY_DEFINITIONS, entity.getName());
+        property.setDate("2020-06-15T10:21:49.123Z");
+        property.setKey(Collections.singletonMap("a", "b"));
+        tags = new HashMap<>();
+        tags.put("roundlot", "20");
+        tags.put("product", "17");
+        tags.put("test", "8");
+        property.setTags(tags);
+        PropertyMethod.insertPropertyCheck(property);
+
+        property = new Property(STATISTICS, entity.getName());
+        property.setDate("2020-06-15T20:21:49.123Z");
+        tags = new HashMap<>();
+        tags.put("numoffers", "1234");
+        tags.put("numbids", "5060");
+        tags.put("bid", "196.04");
+        property.setTags(tags);
+        PropertyMethod.insertPropertyCheck(property);
+        property = new Property(STATISTICS, entity.getName());
+        property.setKey(Collections.singletonMap("c", "d"));
+        property.setDate("2020-06-15T20:25:49.123Z");
+        tags = new HashMap<>();
+        tags.put("numoffers", "0");
+        tags.put("numbids", "0");
+        tags.put("bid", "0");
+        property.setTags(tags);
+        PropertyMethod.insertPropertyCheck(property);
+
+    }
+
+    private String entity() {
+        return symbol() + "_[" + clazz() + "]";
+    }
+
+    @Test
+    public void test() throws Exception {
+        String sql = "select sec_def.roundlot, sec_def('product'), stat.numbids, stat('numoffers'), stat.bid " +
+                "from atsd_trade where " + instrumentCondition();
+        String[][] expected = new String[][]{
+                {
+                        "10", "5", "5060", "1234", "196.04"
+                }
+        };
+        assertSqlQueryRows(expected, sql);
+    }
+}
