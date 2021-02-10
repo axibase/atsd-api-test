f113eb73ba5e8e8d36cc1b3c11ce19dbaf7cd155
Igor <igor.shmagrinsky@axibase.com>
2021-02-10 17:45:39 +0300

    fix npe during serialization

--- src/main/java/com/axibase/tsd/api/method/metric/MetricSeriesParameters.java
+++ src/main/java/com/axibase/tsd/api/method/metric/MetricSeriesParameters.java
@@ -3,23 +3,21 @@ package com.axibase.tsd.api.method.metric;
 import com.axibase.tsd.api.method.MethodParameters;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 
-@Getter
-@Setter
+@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class MetricSeriesParameters extends MethodParameters {
     private String entity;
-    private Map<String, String> tags = new HashMap<>();
+    private Map<String, String> tags;
     private String minInsertDate;
     private String maxInsertDate;
     private Integer limit;
@@ -35,14 +33,16 @@ public class MetricSeriesParameters extends MethodParameters {
     }
 
     public Map<String, String> getTags() {
-        return new HashMap<>(tags);
+        return tags == null ? Collections.emptyMap() : new HashMap<>(tags);
     }
 
     @Override
     public Map<String, Object> toParameterMap() {
         Map<String, Object> parameters = this.toMap();
-        for (Map.Entry<String, String> tag : tags.entrySet()) {
-            parameters.put("tags." + tag.getKey(), tag.getValue());
+        if (tags != null) {
+            for (Map.Entry<String, String> tag : tags.entrySet()) {
+                parameters.put("tags." + tag.getKey(), tag.getValue());
+            }
         }
         parameters.remove("tags");
         return Collections.unmodifiableMap(parameters);
